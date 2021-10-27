e52b2603a74c4b29198e91059d051144376c02b7
Evgenii Stratonikov <evgeniy@nspcc.ru>
2021-10-27 13:00:35 +0300

    [#42] netmap: move package from neofs-api-go
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20210521073959-f0d4d129b7f1
 	github.com/golang/mock v1.6.0
 	github.com/google/uuid v1.2.0
+	github.com/nspcc-dev/hrw v1.0.9
 	github.com/nspcc-dev/neo-go v0.96.1
 	github.com/nspcc-dev/neofs-api-go v1.30.0
 	github.com/stretchr/testify v1.7.0
---
+++ netmap/replica.go
@@ -0,0 +1,71 @@
+package netmap
+
+import (
+	"github.com/nspcc-dev/neofs-api-go/v2/netmap"
+)
+
+// Replica represents v2-compatible object replica descriptor.
+type Replica netmap.Replica
+
+// NewReplica creates and returns new Replica instance.
+//
+// Defaults:
+//  - count: 0;
+//  - selector: "".
+func NewReplica() *Replica {
+	return NewReplicaFromV2(new(netmap.Replica))
+}
+
+// NewReplicaFromV2 converts v2 Replica to Replica.
+//
+// Nil netmap.Replica converts to nil.
+func NewReplicaFromV2(f *netmap.Replica) *Replica {
+	return (*Replica)(f)
+}
+
+// ToV2 converts Replica to v2 Replica.
+//
+// Nil Replica converts to nil.
+func (r *Replica) ToV2() *netmap.Replica {
+	return (*netmap.Replica)(r)
+}
+
+// Count returns number of object replicas.
+func (r *Replica) Count() uint32 {
+	return (*netmap.Replica)(r).GetCount()
+}
+
+// SetCount sets number of object replicas.
+func (r *Replica) SetCount(c uint32) {
+	(*netmap.Replica)(r).SetCount(c)
+}
+
+// Selector returns name of selector bucket to put replicas.
+func (r *Replica) Selector() string {
+	return (*netmap.Replica)(r).GetSelector()
+}
+
+// SetSelector sets name of selector bucket to put replicas.
+func (r *Replica) SetSelector(s string) {
+	(*netmap.Replica)(r).SetSelector(s)
+}
+
+// Marshal marshals Replica into a protobuf binary form.
+func (r *Replica) Marshal() ([]byte, error) {
+	return (*netmap.Replica)(r).StableMarshal(nil)
+}
+
+// Unmarshal unmarshals protobuf binary representation of Replica.
+func (r *Replica) Unmarshal(data []byte) error {
+	return (*netmap.Replica)(r).Unmarshal(data)
+}
+
+// MarshalJSON encodes Replica to protobuf JSON format.
+func (r *Replica) MarshalJSON() ([]byte, error) {
+	return (*netmap.Replica)(r).MarshalJSON()
+}
+
+// UnmarshalJSON decodes Replica from protobuf JSON format.
+func (r *Replica) UnmarshalJSON(data []byte) error {
+	return (*netmap.Replica)(r).UnmarshalJSON(data)
+}
