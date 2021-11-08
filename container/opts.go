73e1d73ba21f4f6c54e872fd48b4eb980cb8f66b
Evgenii Stratonikov <evgeniy@nspcc.ru>
2021-11-08 15:25:13 +0300

    [#60] container: rename `NewOption` and make code more consistent
    In all other places we use `Option` as `func`, there is no need in
    additional indirection.
    
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- container/opts.go
+++ container/opts.go
@@ -8,9 +8,7 @@ import (
 )
 
 type (
-	NewOption interface {
-		apply(*containerOptions)
-	}
+	Option func(*containerOptions)
 
 	containerOptions struct {
 		acl        uint32
@@ -33,72 +31,58 @@ func defaultContainerOptions() containerOptions {
 	}
 }
 
-type funcContainerOption struct {
-	f func(*containerOptions)
-}
-
-func (fco *funcContainerOption) apply(co *containerOptions) {
-	fco.f(co)
-}
-
-func newFuncContainerOption(f func(option *containerOptions)) *funcContainerOption {
-	return &funcContainerOption{
-		f: f,
+func WithPublicBasicACL() Option {
+	return func(option *containerOptions) {
+		option.acl = acl.PublicBasicRule
 	}
 }
 
-func WithPublicBasicACL() NewOption {
-	return newFuncContainerOption(func(option *containerOptions) {
-		option.acl = acl.PublicBasicRule
-	})
-}
-
-func WithReadOnlyBasicACL() NewOption {
-	return newFuncContainerOption(func(option *containerOptions) {
+func WithReadOnlyBasicACL() Option {
+	return func(option *containerOptions) {
 		option.acl = acl.ReadOnlyBasicRule
-	})
+	}
 }
 
-func WithCustomBasicACL(acl uint32) NewOption {
-	return newFuncContainerOption(func(option *containerOptions) {
+func WithCustomBasicACL(acl uint32) Option {
+	return func(option *containerOptions) {
 		option.acl = acl
-	})
+	}
 }
 
-func WithNonce(nonce uuid.UUID) NewOption {
-	return newFuncContainerOption(func(option *containerOptions) {
+func WithNonce(nonce uuid.UUID) Option {
+	return func(option *containerOptions) {
 		option.nonce = nonce
-	})
+	}
 }
 
-func WithOwnerID(id *owner.ID) NewOption {
-	return newFuncContainerOption(func(option *containerOptions) {
+func WithOwnerID(id *owner.ID) Option {
+	return func(option *containerOptions) {
 		option.owner = id
-	})
+	}
 }
 
-func WithNEO3Wallet(w *owner.NEO3Wallet) NewOption {
-	return newFuncContainerOption(func(option *containerOptions) {
+func WithNEO3Wallet(w *owner.NEO3Wallet) Option {
+	return func(option *containerOptions) {
 		if option.owner == nil {
 			option.owner = new(owner.ID)
 		}
 
 		option.owner.SetNeo3Wallet(w)
-	})
+	}
 }
 
-func WithPolicy(policy *netmap.PlacementPolicy) NewOption {
-	return newFuncContainerOption(func(option *containerOptions) {
+func WithPolicy(policy *netmap.PlacementPolicy) Option {
+	return func(option *containerOptions) {
 		option.policy = policy
-	})
+	}
 }
 
-func WithAttribute(key, value string) NewOption {
-	return newFuncContainerOption(func(option *containerOptions) {
+func WithAttribute(key, value string) Option {
+	return func(option *containerOptions) {
 		attr := NewAttribute()
 		attr.SetKey(key)
 		attr.SetValue(value)
 
 		option.attributes = append(option.attributes, attr)
-	})
+	}
 }
