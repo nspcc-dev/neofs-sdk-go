db784a57589767344625888d0227ee0d2dbe6be3
Evgenii Stratonikov <evgeniy@nspcc.ru>
2021-11-08 15:13:59 +0300

    [#59] audit: move package from neofs-api-go
    Close #59.
    
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

---
+++ audit/test/generate.go
@@ -0,0 +1,37 @@
+package audittest
+
+import (
+	"github.com/nspcc-dev/neofs-sdk-go/audit"
+	cidtest "github.com/nspcc-dev/neofs-sdk-go/container/id/test"
+	"github.com/nspcc-dev/neofs-sdk-go/object"
+	objecttest "github.com/nspcc-dev/neofs-sdk-go/object/test"
+	"github.com/nspcc-dev/neofs-sdk-go/version"
+)
+
+// Generate returns random audit.Result.
+func Generate() *audit.Result {
+	x := audit.NewResult()
+
+	x.SetVersion(version.Current())
+	x.SetContainerID(cidtest.GenerateID())
+	x.SetPublicKey([]byte("key"))
+	x.SetComplete(true)
+	x.SetAuditEpoch(44)
+	x.SetHit(55)
+	x.SetMiss(66)
+	x.SetFail(77)
+	x.SetRetries(88)
+	x.SetRequests(99)
+	x.SetFailNodes([][]byte{
+		[]byte("node1"),
+		[]byte("node2"),
+	})
+	x.SetPassNodes([][]byte{
+		[]byte("node3"),
+		[]byte("node4"),
+	})
+	x.SetPassSG([]*object.ID{objecttest.ID(), objecttest.ID()})
+	x.SetFailSG([]*object.ID{objecttest.ID(), objecttest.ID()})
+
+	return x
+}
