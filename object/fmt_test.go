39d3317ef60cc3d1e056e9eac275fa13edf441b9
Evgenii Stratonikov <evgeniy@nspcc.ru>
2021-11-08 13:04:45 +0300

    [#64] object: move package from neofs-api-go
    Also, remove deprecated method.
    
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

---
+++ object/fmt_test.go
@@ -0,0 +1,81 @@
+package object
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVerificationFields(t *testing.T) {
+	obj := NewRaw()
+
+	payload := make([]byte, 10)
+	_, _ = rand.Read(payload)
+
+	obj.SetPayload(payload)
+	obj.SetPayloadSize(uint64(len(payload)))
+
+	p, err := keys.NewPrivateKey()
+	require.NoError(t, err)
+	require.NoError(t, SetVerificationFields(&p.PrivateKey, obj))
+
+	require.NoError(t, CheckVerificationFields(obj.Object()))
+
+	items := []struct {
+		corrupt func()
+		restore func()
+	}{
+		{
+			corrupt: func() {
+				payload[0]++
+			},
+			restore: func() {
+				payload[0]--
+			},
+		},
+		{
+			corrupt: func() {
+				obj.SetPayloadSize(obj.PayloadSize() + 1)
+			},
+			restore: func() {
+				obj.SetPayloadSize(obj.PayloadSize() - 1)
+			},
+		},
+		{
+			corrupt: func() {
+				obj.ID().ToV2().GetValue()[0]++
+			},
+			restore: func() {
+				obj.ID().ToV2().GetValue()[0]--
+			},
+		},
+		{
+			corrupt: func() {
+				obj.Signature().Key()[0]++
+			},
+			restore: func() {
+				obj.Signature().Key()[0]--
+			},
+		},
+		{
+			corrupt: func() {
+				obj.Signature().Sign()[0]++
+			},
+			restore: func() {
+				obj.Signature().Sign()[0]--
+			},
+		},
+	}
+
+	for _, item := range items {
+		item.corrupt()
+
+		require.Error(t, CheckVerificationFields(obj.Object()))
+
+		item.restore()
+
+		require.NoError(t, CheckVerificationFields(obj.Object()))
+	}
+}
---
+++ object/splitinfo.go
@@ -0,0 +1,66 @@
+package object
+
+import (
+	"github.com/nspcc-dev/neofs-api-go/v2/object"
+)
+
+type SplitInfo object.SplitInfo
+
+// NewSplitInfoFromV2 wraps v2 SplitInfo message to SplitInfo.
+//
+// Nil object.SplitInfo converts to nil.
+func NewSplitInfoFromV2(v2 *object.SplitInfo) *SplitInfo {
+	return (*SplitInfo)(v2)
+}
+
+// NewSplitInfo creates and initializes blank SplitInfo.
+//
+// Defaults:
+//  - splitID: nil;
+//  - lastPart nil;
+//  - link: nil.
+func NewSplitInfo() *SplitInfo {
+	return NewSplitInfoFromV2(new(object.SplitInfo))
+}
+
+// ToV2 converts SplitInfo to v2 SplitInfo message.
+//
+// Nil SplitInfo converts to nil.
+func (s *SplitInfo) ToV2() *object.SplitInfo {
+	return (*object.SplitInfo)(s)
+}
+
+func (s *SplitInfo) SplitID() *SplitID {
+	return NewSplitIDFromV2(
+		(*object.SplitInfo)(s).GetSplitID())
+}
+
+func (s *SplitInfo) SetSplitID(v *SplitID) {
+	(*object.SplitInfo)(s).SetSplitID(v.ToV2())
+}
+
+func (s *SplitInfo) LastPart() *ID {
+	return NewIDFromV2(
+		(*object.SplitInfo)(s).GetLastPart())
+}
+
+func (s *SplitInfo) SetLastPart(v *ID) {
+	(*object.SplitInfo)(s).SetLastPart(v.ToV2())
+}
+
+func (s *SplitInfo) Link() *ID {
+	return NewIDFromV2(
+		(*object.SplitInfo)(s).GetLink())
+}
+
+func (s *SplitInfo) SetLink(v *ID) {
+	(*object.SplitInfo)(s).SetLink(v.ToV2())
+}
+
+func (s *SplitInfo) Marshal() ([]byte, error) {
+	return (*object.SplitInfo)(s).StableMarshal(nil)
+}
+
+func (s *SplitInfo) Unmarshal(data []byte) error {
+	return (*object.SplitInfo)(s).Unmarshal(data)
+}
