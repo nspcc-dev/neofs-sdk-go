b7d7e296252d7f84d3998c53bc45875007ac56cf
Denis Kirillov <denis@nspcc.ru>
2022-02-17 15:27:49 +0300

    [#137] Use callback response to update epoch
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- client/response.go
+++ client/response.go
@@ -5,6 +5,8 @@ import "github.com/nspcc-dev/neofs-api-go/v2/session"
 // ResponseMetaInfo groups meta information about any NeoFS API response.
 type ResponseMetaInfo struct {
 	key []byte
+
+	epoch uint64
 }
 
 type responseV2 interface {
@@ -17,4 +19,9 @@ type responseV2 interface {
 // Result must not be mutated.
 func (x ResponseMetaInfo) ResponderKey() []byte {
 	return x.key
+}
+
+// Epoch return current epoch.
+func (x ResponseMetaInfo) Epoch() uint64 {
+	return x.epoch
 }
