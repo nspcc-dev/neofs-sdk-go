df0573d52147685a43af0c99789c6fe65db8df12
Denis Kirillov <denis@nspcc.ru>
2022-03-24 17:35:09 +0300

    [#137] pool: drop sessionTokenThreshold
    Description
    
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- pool/cache_test.go
+++ pool/cache_test.go
@@ -2,39 +2,12 @@ package pool
 
 import (
 	"testing"
-	"time"
 
 	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
 	"github.com/nspcc-dev/neofs-sdk-go/session"
 	sessiontest "github.com/nspcc-dev/neofs-sdk-go/session/test"
 	"github.com/stretchr/testify/require"
 )
-
-func TestSessionCache_GetAccessTime(t *testing.T) {
-	const key = "Foo"
-
-	cache, err := newCache()
-	require.NoError(t, err)
-
-	st := session.NewToken()
-	st.SetExp(1)
-
-	cache.Put(key, st)
-
-	t1, ok := cache.GetAccessTime(key)
-	require.True(t, ok)
-
-	time.Sleep(10 * time.Millisecond)
-
-	t2, ok := cache.GetAccessTime(key)
-	require.True(t, ok)
-	require.Equal(t, t1, t2)
-
-	_ = cache.Get(key)
-	t3, ok := cache.GetAccessTime(key)
-	require.True(t, ok)
-	require.NotEqual(t, t1, t3)
-}
 
 func TestSessionCache_GetUnmodifiedToken(t *testing.T) {
 	const key = "Foo"
