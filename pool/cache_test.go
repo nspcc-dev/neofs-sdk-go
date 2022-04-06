f38a24e8b581e74758041f369cb98b842ce00f8b
Alex Vanin <alexey@nspcc.ru>
2022-04-06 11:03:41 +0300

    [#193] pool: Add test to check side effects in token cache
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- pool/cache_test.go
+++ pool/cache_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
+	"github.com/nspcc-dev/neofs-sdk-go/session"
+	sessiontest "github.com/nspcc-dev/neofs-sdk-go/session/test"
 	"github.com/stretchr/testify/require"
 )
 
@@ -28,4 +31,33 @@ func TestSessionCache_GetAccessTime(t *testing.T) {
 	t3, ok := cache.GetAccessTime(key)
 	require.True(t, ok)
 	require.NotEqual(t, t1, t3)
+}
+
+func TestSessionCache_GetUnmodifiedToken(t *testing.T) {
+	const key = "Foo"
+	target := sessiontest.Token()
+
+	pk, err := keys.NewPrivateKey()
+	require.NoError(t, err)
+
+	check := func(t *testing.T, tok *session.Token, extra string) {
+		require.Empty(t, tok.Signature().Sign(), extra)
+		require.Nil(t, tok.Context(), extra)
+	}
+
+	cache, err := newCache()
+	require.NoError(t, err)
+
+	cache.Put(key, target)
+	value := cache.Get(key)
+	check(t, value, "before sign")
+
+	err = value.Sign(&pk.PrivateKey)
+	require.NoError(t, err)
+
+	octx := sessiontest.ObjectContext()
+	value.SetContext(octx)
+
+	value = cache.Get(key)
+	check(t, value, "after sign")
 }
