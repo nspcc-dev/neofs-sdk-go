e3d74e7f73b2815e17d0b7f781d215ba66022c7a
Leonard Lyubich <leonard@nspcc.ru>
2022-04-19 11:22:43 +0300

    [#190] crypto/ecdsa: Actualize package docs
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- crypto/ecdsa/doc.go
+++ crypto/ecdsa/doc.go
@@ -1,7 +1,9 @@
 /*
 Package neofsecdsa collects ECDSA primitives for NeoFS cryptography.
 
-Signer and PublicKey provide corresponding interfaces from neofscrypto package.
+Signer and PublicKey support ECDSA signature algorithm with SHA-512 hashing.
+SignerRFC6979 and PublicKeyRFC6979 implement signature algorithm described in RFC 6979.
+All these types provide corresponding interfaces from neofscrypto package.
 
 Package import causes registration of next signature schemes via neofscrypto.RegisterScheme:
   - neofscrypto.ECDSA_SHA512
