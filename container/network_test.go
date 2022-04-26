ac76391d125025321c1ac2f016a0e96fbb463936
Pavel Karpy <carpawell@nspcc.ru>
2022-04-26 18:01:40 +0300

    [#240] container: Support disabling homomorphic hashing
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

---
+++ container/network_test.go
@@ -0,0 +1,33 @@
+package container_test
+
+import (
+	"testing"
+
+	"github.com/nspcc-dev/neofs-sdk-go/container"
+	containertest "github.com/nspcc-dev/neofs-sdk-go/container/test"
+	netmaptest "github.com/nspcc-dev/neofs-sdk-go/netmap/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestContainer_NetworkConfig(t *testing.T) {
+	c := containertest.Container()
+	nc := netmaptest.NetworkInfo()
+
+	t.Run("default", func(t *testing.T) {
+		require.False(t, container.IsHomomorphicHashingDisabled(c))
+
+		res := container.AssertNetworkConfig(c, nc)
+
+		require.True(t, res)
+	})
+
+	nc.DisableHomomorphicHashing()
+
+	t.Run("apply", func(t *testing.T) {
+		require.False(t, container.IsHomomorphicHashingDisabled(c))
+
+		container.ApplyNetworkConfig(&c, nc)
+
+		require.True(t, container.IsHomomorphicHashingDisabled(c))
+	})
+}
