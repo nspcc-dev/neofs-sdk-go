40942affe92a2596309e8393d23663ac2a8d900d
Leonard Lyubich <leonard@nspcc.ru>
2022-06-27 10:24:17 +0300

    [#278] client: Add unit test for error checkers
    `IsErr*` functions assert `error` argument corresponds to particular
    typed error.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

---
+++ client/errors_test.go
@@ -0,0 +1,44 @@
+package client_test
+
+import (
+	"testing"
+
+	"github.com/nspcc-dev/neofs-sdk-go/client"
+	apistatus "github.com/nspcc-dev/neofs-sdk-go/client/status"
+	"github.com/stretchr/testify/require"
+)
+
+func TestErrors(t *testing.T) {
+	for _, tc := range []struct {
+		check func(error) bool
+		errs  []error
+	}{
+		{
+			check: client.IsErrContainerNotFound,
+			errs: []error{
+				apistatus.ContainerNotFound{},
+				new(apistatus.ContainerNotFound),
+			},
+		},
+		{
+			check: client.IsErrObjectNotFound,
+			errs: []error{
+				apistatus.ObjectNotFound{},
+				new(apistatus.ObjectNotFound),
+			},
+		},
+		{
+			check: client.IsErrObjectAlreadyRemoved,
+			errs: []error{
+				apistatus.ObjectAlreadyRemoved{},
+				new(apistatus.ObjectAlreadyRemoved),
+			},
+		},
+	} {
+		require.NotEmpty(t, tc.errs)
+
+		for i := range tc.errs {
+			require.True(t, tc.check(tc.errs[i]), tc.errs[i])
+		}
+	}
+}
