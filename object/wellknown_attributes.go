881ec8a6d6086320a8acc73aa731a00c4d285aae
Denis Kirillov <denis@nspcc.ru>
2022-09-06 11:50:44 +0300

    [#215] object: Add well-known FilePath attribute
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- object/wellknown_attributes.go
+++ object/wellknown_attributes.go
@@ -9,6 +9,15 @@ const (
 	// file name to be associated with the object on saving.
 	AttributeFileName = "FileName"
 
+	// AttributeFilePath is an attribute key that is commonly used to denote
+	// full path to be associated with the object on saving. Should start with a
+	// '/' and use '/' as a delimiting symbol. Trailing '/' should be
+	// interpreted as a virtual directory marker. If an object has conflicting
+	// FilePath and FileName, FilePath should have higher priority, because it
+	// is used to construct the directory tree. FilePath with trailing '/' and
+	// non-empty FileName attribute should not be used together.
+	AttributeFilePath = "FilePath"
+
 	// AttributeTimestamp is an attribute key that is commonly used to denote
 	// user-defined local time of object creation in Unix Timestamp format.
 	AttributeTimestamp = "Timestamp"
