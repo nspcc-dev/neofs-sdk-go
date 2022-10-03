452a50e9d579dd95c7e5e02bbaa35c0ecdcf41c8
Leonard Lyubich <ctulhurider@gmail.com>
2022-10-03 14:13:48 +0400

    [#343] client: Accept context parameter in `Dial`
    In previous implementation of `Client.Dial` there was no ability to
    specify parent context (e.g. global application context).
    
    Add `PrmDial.SetContext` method which accepts optional base dial
    context. Use the context to open client connection or fall back to using
    `context.Background()`.
    
    Upgraded version of `github.com/nspcc-dev/neofs-api-go/v2` module
    also fixes the problem when dial timeout didn't work properly.
    
    Signed-off-by: Leonard Lyubich <ctulhurider@gmail.com>

--- client/client_test.go
+++ client/client_test.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"context"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
@@ -36,4 +37,32 @@ func newClient(server neoFSAPIServer) *Client {
 	c.setNeoFSAPIServer(server)
 
 	return &c
+}
+
+func TestClient_DialContext(t *testing.T) {
+	var c Client
+
+	// try to connect to any host
+	var prm PrmDial
+	prm.SetServerURI("localhost:8080")
+
+	assert := func(ctx context.Context, errExpected error) {
+		// use the particular context
+		prm.SetContext(ctx)
+
+		// expect particular context error according to Dial docs
+		require.ErrorIs(t, c.Dial(prm), errExpected)
+	}
+
+	// create pre-abandoned context
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	assert(ctx, context.Canceled)
+
+	// create "pre-deadlined" context
+	ctx, cancel = context.WithTimeout(context.Background(), 0)
+	defer cancel()
+
+	assert(ctx, context.DeadlineExceeded)
 }
