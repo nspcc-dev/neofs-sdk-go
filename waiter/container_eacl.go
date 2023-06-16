aeec4eb4753663fe7c1f3a8bfac00b851befad45
Evgenii Baidakov <evgenii@nspcc.io>
2023-06-16 11:43:26 +0400

    waiter: Add Waiter for async operations
    close #444
    
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

---
+++ waiter/container_eacl.go
@@ -0,0 +1,82 @@
+package waiter
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/nspcc-dev/neofs-sdk-go/client"
+	apistatus "github.com/nspcc-dev/neofs-sdk-go/client/status"
+	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+	"github.com/nspcc-dev/neofs-sdk-go/eacl"
+)
+
+// ContainerSetEACLExecutor represents requirements to async container setEACL operation.
+// See documentation for functions in [client.Client]. The same semantics is expected.
+type ContainerSetEACLExecutor interface {
+	ContainerSetEACL(ctx context.Context, table eacl.Table, prm client.PrmContainerSetEACL) error
+	ContainerEACL(ctx context.Context, id cid.ID, prm client.PrmContainerEACL) (eacl.Table, error)
+}
+
+// ContainerSetEACLWaiter implements sync logic to container setEACL operation.
+type ContainerSetEACLWaiter struct {
+	executor     ContainerSetEACLExecutor
+	pollInterval time.Duration
+}
+
+// NewContainerSetEACLWaiter is a constructor for NewContainerSetEACLWaiter.
+func NewContainerSetEACLWaiter(c ContainerSetEACLExecutor, pollInterval time.Duration) ContainerSetEACLWaiter {
+	return ContainerSetEACLWaiter{executor: c, pollInterval: pollInterval}
+}
+
+// SetPollInterval allows rewrite default poll interval.
+func (w *ContainerSetEACLWaiter) SetPollInterval(interval time.Duration) {
+	w.pollInterval = interval
+}
+
+// ContainerSetEACL sends request to update eACL table of the NeoFS container.
+//
+// See documentation for function in [client.Client.ContainerSetEACL].
+func (w ContainerSetEACLWaiter) ContainerSetEACL(ctx context.Context, table eacl.Table, prm client.PrmContainerSetEACL) error {
+	if err := w.executor.ContainerSetEACL(ctx, table, prm); err != nil {
+		return fmt.Errorf("container setEacl: %w", err)
+	}
+
+	contID, ok := table.CID()
+	if !ok {
+		return client.ErrMissingEACLContainer
+	}
+
+	newBinary, err := table.Marshal()
+	if err != nil {
+		return fmt.Errorf("newTable.Marshal: %w", err)
+	}
+
+	var prmEacl client.PrmContainerEACL
+
+	logic := func() error {
+		actualTable, err := w.executor.ContainerEACL(ctx, contID, prmEacl)
+		if err != nil {
+			if errors.Is(err, apistatus.ErrEACLNotFound) {
+				return errRetry
+			}
+
+			return fmt.Errorf("ContainerEACL: %w", err)
+		}
+
+		actualBinary, err := actualTable.Marshal()
+		if err != nil {
+			return fmt.Errorf("table.Marshal: %w", err)
+		}
+
+		if bytes.Equal(newBinary, actualBinary) {
+			return nil
+		}
+
+		return errRetry
+	}
+
+	return poll(ctx, w.pollInterval, logic)
+}
---
+++ waiter/example_waiter_test.go
@@ -0,0 +1,80 @@
+package waiter
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
+	"github.com/nspcc-dev/neofs-sdk-go/client"
+	"github.com/nspcc-dev/neofs-sdk-go/container"
+	"github.com/nspcc-dev/neofs-sdk-go/container/acl"
+	neofsecdsa "github.com/nspcc-dev/neofs-sdk-go/crypto/ecdsa"
+	"github.com/nspcc-dev/neofs-sdk-go/netmap"
+	"github.com/nspcc-dev/neofs-sdk-go/pool"
+	"github.com/nspcc-dev/neofs-sdk-go/user"
+)
+
+func ExampleNewWaiter() {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// The account was taken from https://github.com/nspcc-dev/neofs-aio.
+	key, err := keys.NEP2Decrypt("6PYM8VdX2BSm7BSXKzV4Fz6S3R9cDLLWNrD9nMjxW352jEv3fsC8N3wNLY", "one", keys.NEP2ScryptParams())
+	if err != nil {
+		panic(err)
+	}
+
+	signer := neofsecdsa.SignerRFC6979(key.PrivateKey)
+
+	var account user.ID
+	var cont container.Container
+	var pp netmap.PlacementPolicy
+	var rd netmap.ReplicaDescriptor
+
+	// prepare container.
+	cont.Init()
+	cont.SetBasicACL(acl.PublicRW)
+	cont.SetOwner(account)
+	cont.SetName(strconv.FormatInt(time.Now().UnixNano(), 16))
+	cont.SetCreationTime(time.Now())
+
+	// prepare placement policy.
+	pp.SetContainerBackupFactor(1)
+	rd.SetNumberOfObjects(1)
+	pp.AddReplicas(rd)
+	cont.SetPlacementPolicy(pp)
+
+	// prepare pool.
+	opts := pool.InitParameters{}
+	opts.SetSigner(signer)
+	opts.SetClientRebalanceInterval(30 * time.Second)
+	opts.AddNode(pool.NewNodeParam(1, "grpc://localhost:8080", 1))
+
+	// init pool.
+	p, err := pool.NewPool(opts)
+	if err != nil {
+		panic(err)
+	}
+
+	// create waiter.
+	wait := NewWaiter(p, DefaultPollInterval)
+
+	var prmPut client.PrmContainerPut
+	prmPut.SetSigner(signer)
+
+	// Waiter creates container and wait until container created or context timeout.
+	containerID, err := wait.ContainerPut(ctx, cont, prmPut)
+	if err != nil {
+		panic(err)
+	}
+
+	var prmDelete client.PrmContainerDelete
+	prmDelete.SetSigner(signer)
+
+	// Waiter deletes container and wait until container deleted or context timeout.
+	err = wait.ContainerDelete(ctx, containerID, prmDelete)
+	if err != nil {
+		panic(err)
+	}
+}
