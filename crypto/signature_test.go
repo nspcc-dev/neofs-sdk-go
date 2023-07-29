22241275bc39f0899f52836341f99f220e71d9c0
Leonard Lyubich <leonard@morphbits.io>
2023-07-29 07:33:15 +0400

    crypto: Extend Signature type
    Provide getters of signature scheme, public key and value. Make
    `WriteToV2` method to decode public key from binary to assert scheme
    format.
    
    New API will allow to avoid `WriteToV2` calls in user space. New unit
    test checks and shows client-to-server transport over NeoFS API V2
    protocol.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

---
+++ crypto/signature_test.go
@@ -0,0 +1,69 @@
+package neofscrypto_test
+
+import (
+	"testing"
+
+	"github.com/nspcc-dev/neofs-api-go/v2/refs"
+	neofscrypto "github.com/nspcc-dev/neofs-sdk-go/crypto"
+	"github.com/nspcc-dev/neofs-sdk-go/crypto/test"
+	"github.com/stretchr/testify/require"
+)
+
+const anyUnsupportedScheme = neofscrypto.ECDSA_WALLETCONNECT + 1
+
+func TestSignatureLifecycle(t *testing.T) {
+	data := []byte("Hello, world!")
+	signer := test.RandomSigner(t)
+	scheme := signer.Scheme()
+	pubKey := signer.Public()
+	bPubKey := make([]byte, pubKey.MaxEncodedSize())
+	bPubKey = bPubKey[:pubKey.Encode(bPubKey)]
+
+	var clientSig neofscrypto.Signature
+
+	err := clientSig.Calculate(signer, data)
+	require.NoError(t, err)
+
+	testSig := func(sig neofscrypto.Signature) {
+		require.Equal(t, signer.Scheme(), sig.Scheme())
+		require.Equal(t, signer.Public(), sig.PublicKey())
+		require.Equal(t, bPubKey, sig.PublicKeyBytes())
+		require.NotEmpty(t, sig.Value())
+		require.True(t, sig.Verify(data))
+	}
+
+	testSig(clientSig)
+
+	var sigV2 refs.Signature
+	clientSig.WriteToV2(&sigV2)
+
+	require.Equal(t, refs.SignatureScheme(scheme), sigV2.GetScheme())
+	require.Equal(t, bPubKey, sigV2.GetKey())
+	require.Equal(t, clientSig.Value(), sigV2.GetSign())
+
+	// sigV2 transmitted to server over the network
+
+	var serverSig neofscrypto.Signature
+
+	err = serverSig.ReadFromV2(sigV2)
+	require.NoError(t, err)
+
+	testSig(serverSig)
+
+	// break the message in different ways
+	for i, breakSig := range []func(*refs.Signature){
+		func(sigV2 *refs.Signature) { sigV2.SetScheme(refs.SignatureScheme(anyUnsupportedScheme)) },
+		func(sigV2 *refs.Signature) {
+			key := sigV2.GetKey()
+			sigV2.SetKey(key[:len(key)-1])
+		},
+		func(sigV2 *refs.Signature) { sigV2.SetKey(append(sigV2.GetKey(), 1)) },
+		func(sigV2 *refs.Signature) { sigV2.SetSign(nil) },
+	} {
+		sigV2Cp := sigV2
+		breakSig(&sigV2Cp)
+
+		err = serverSig.ReadFromV2(sigV2Cp)
+		require.Errorf(t, err, "break func #%d", i)
+	}
+}
