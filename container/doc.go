26798ab3496fc108aa832a326c9fd9ed750feb6a
Evgenii Baidakov <evgenii@nspcc.io>
2023-08-17 12:52:28 +0400

    container: Remove code examples from comments
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

--- container/doc.go
+++ container/doc.go
@@ -1,49 +1,6 @@
 /*
 Package container provides functionality related to the NeoFS containers.
 
-The base type is Container. To create new container in the NeoFS network
-Container instance should be initialized
-
-	var cnr Container
-	cnr.Init()
-	// fill all the fields
-
-	// encode cnr and send
-
-After the container is persisted in the NeoFS network, applications can process
-it using the instance of Container types
-
-	// recv binary container
-
-	var cnr Container
-
-	err := cnr.Unmarshal(bin)
-	// ...
-
-	// process the container data
-
-Instances can be also used to process NeoFS API V2 protocol messages
-(see neo.fs.v2.container package in https://github.com/nspcc-dev/neofs-api).
-
-On client side:
-
-	import "github.com/nspcc-dev/neofs-api-go/v2/container"
-
-	var msg container.Container
-	cnr.WriteToV2(&msg)
-
-	// send msg
-
-On server side:
-
-	// recv msg
-
-	var cnr Container
-	cnr.ReadFromV2(msg)
-
-	// process cnr
-
-Using package types in an application is recommended to potentially work with
-different protocol versions with which these types are compatible.
+The base type is [Container].
 */
 package container
---
+++ container/example_test.go
@@ -0,0 +1,58 @@
+package container_test
+
+import (
+	"time"
+
+	apiGoContainer "github.com/nspcc-dev/neofs-api-go/v2/container"
+	"github.com/nspcc-dev/neofs-sdk-go/container"
+	"github.com/nspcc-dev/neofs-sdk-go/container/acl"
+	"github.com/nspcc-dev/neofs-sdk-go/netmap"
+	"github.com/nspcc-dev/neofs-sdk-go/user"
+)
+
+// To create new container in the NeoFS network Container instance should be initialized.
+func ExampleContainer_Init() {
+	// import "github.com/nspcc-dev/neofs-sdk-go/container/acl"
+	// import "github.com/nspcc-dev/neofs-sdk-go/user"
+	// import "github.com/nspcc-dev/neofs-sdk-go/netmap"
+
+	var account user.ID
+
+	var cnr container.Container
+	cnr.Init()
+
+	// required fields
+	cnr.SetOwner(account)
+	cnr.SetBasicACL(acl.PublicRWExtended)
+
+	// optional
+	cnr.SetName("awesome container name")
+	cnr.SetCreationTime(time.Now())
+	// ...
+
+	var rd netmap.ReplicaDescriptor
+	rd.SetNumberOfObjects(1)
+
+	// placement policy and replicas definition is required
+	var pp netmap.PlacementPolicy
+	pp.SetContainerBackupFactor(1)
+	pp.AddReplicas(rd)
+
+	cnr.SetPlacementPolicy(pp)
+}
+
+// Instances can be also used to process NeoFS API V2 protocol messages with [https://github.com/nspcc-dev/neofs-api] package.
+func ExampleContainer_marshalling() {
+	// import apiGoContainer "github.com/nspcc-dev/neofs-api-go/v2/container"
+
+	// On the client side.
+
+	var cnr container.Container
+	var msg apiGoContainer.Container
+	cnr.WriteToV2(&msg)
+	// *send message*
+
+	// On the server side.
+
+	_ = cnr.ReadFromV2(msg)
+}
