38bcaa7fd30d527a753c5706c0cffc0a1c419bee
Evgenii Baidakov <evgenii@nspcc.io>
2023-08-22 08:13:39 +0400

    object: Update documentation
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

--- object/id/doc.go
+++ object/id/doc.go
@@ -1,10 +1,7 @@
 /*
 Package oid provides primitives to work with object identification in NeoFS.
 
-Address type is used for global object identity inside the NeoFS network,
-while ID represents identity within a fixed container.
-
-Using package types in an application is recommended to potentially work with
-different protocol versions with which these types are compatible.
+[Address] type is used for global object identity inside the NeoFS network,
+while [ID] represents identity within a fixed container.
 */
 package oid
