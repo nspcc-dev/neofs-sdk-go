944c41b7c05ce56f44d889092af68caca8ca991e
Leonard Lyubich <leonard@morphbits.io>
2023-08-30 19:25:56 +0400

    object: Add getter of user attributes only
    Sometimes it's needed to exclude system attributes in which only the
    root part of the system itself is interested. Previously, there was no
    convenient API to distinguish system attributes from the other ones.
    
    Add `UserAttributes` method that works similar to presented earlier
    `Attributes` but excludes system attributes.
    
    Refs #314.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- object/object_test.go
+++ object/object_test.go
@@ -1,6 +1,7 @@
 package object_test
 
 import (
+	"strconv"
 	"testing"
 
 	cidtest "github.com/nspcc-dev/neofs-sdk-go/container/id/test"
@@ -23,4 +24,45 @@ func TestInitCreation(t *testing.T) {
 	require.True(t, set)
 	require.Equal(t, cnr, cID)
 	require.Equal(t, &own, o.OwnerID())
+}
+
+func TestObject_UserAttributes(t *testing.T) {
+	var obj object.Object
+	var attrs []object.Attribute
+	mSys := make(map[string]string)
+	mUsr := make(map[string]string)
+
+	for i := 0; i < 10; i++ {
+		si := strconv.Itoa(i)
+
+		keyUsr := "key" + si
+		valUsr := "val" + si
+		keySys := "__NEOFS__" + si
+		valSys := "sys-val" + si
+
+		mUsr[keyUsr] = valUsr
+		mSys[keySys] = valSys
+
+		var aUsr object.Attribute
+		aUsr.SetKey(keyUsr)
+		aUsr.SetValue(valUsr)
+
+		var aSys object.Attribute
+		aSys.SetKey(keySys)
+		aSys.SetValue(valSys)
+
+		attrs = append(attrs, aSys, aUsr)
+	}
+
+	obj.SetAttributes(attrs...)
+
+	for _, a := range obj.UserAttributes() {
+		key, val := a.Key(), a.Value()
+		_, isSys := mSys[key]
+		require.False(t, isSys, key)
+		require.Equal(t, mUsr[key], val, key)
+		delete(mUsr, key)
+	}
+
+	require.Empty(t, mUsr)
 }
