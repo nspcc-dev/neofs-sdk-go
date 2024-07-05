091a279ff2af02d41218b256bef527609ffd9ed7
Leonard Lyubich <leonard@morphbits.io>
2024-07-05 09:27:00 +0400

    checksum: Drop redundant example test
    It teaches what a regular user should be freed from.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- checksum/example_test.go
+++ checksum/example_test.go
@@ -1,14 +1,5 @@
 package checksum
 
-import (
-	"bytes"
-	"crypto/sha256"
-	"fmt"
-	"math/rand"
-
-	"github.com/nspcc-dev/neofs-api-go/v2/refs"
-)
-
 func ExampleCalculate() {
 	payload := []byte{0, 1, 2, 3, 4, 5, 6}
 	var checksum Checksum
@@ -18,30 +9,4 @@ func ExampleCalculate() {
 
 	// checksum contains TZ hash of the payload
 	Calculate(&checksum, TZ, payload)
-}
-
-// Instances can be also used to process NeoFS API V2 protocol messages with [https://github.com/nspcc-dev/neofs-api] package.
-func ExampleChecksum_marshalling() {
-	var (
-		csRaw [sha256.Size]byte
-		csV2  refs.Checksum
-		cs    Checksum
-	)
-
-	//nolint:staticcheck
-	rand.Read(csRaw[:])
-	cs.SetSHA256(csRaw)
-
-	// On the client side.
-
-	cs.WriteToV2(&csV2)
-
-	fmt.Println(bytes.Equal(cs.Value(), csV2.GetSum()))
-	// Example output: true
-
-	// *send message*
-
-	// On the server side.
-
-	_ = cs.ReadFromV2(csV2)
 }
