578361d9fac8b7fcd9dc36ca8bd3b2e6d17e7c7e
Leonard Lyubich <leonard@morphbits.io>
2024-07-12 11:30:47 +0400

    user/id: Split string encoding/decoding tests and cover more cases
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- user/id_test.go
+++ user/id_test.go
@@ -5,7 +5,6 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/mr-tron/base58"
 	"github.com/nspcc-dev/neo-go/pkg/util"
 	"github.com/nspcc-dev/neofs-api-go/v2/refs"
 	"github.com/nspcc-dev/neofs-sdk-go/user"
@@ -93,22 +92,47 @@ func TestV2_ID(t *testing.T) {
 }
 
 func TestID_EncodeToString(t *testing.T) {
-	id := usertest.ID()
+	const s = "NXWcEedga62wcBmfb9dPwar3vbbZJrMtT1"
+	b := [user.IDSize]byte{53, 127, 54, 116, 58, 70, 206, 247, 185, 103, 214, 89, 184, 42, 40, 234, 173, 68, 209, 25, 168, 14, 134, 47, 224}
+	var id user.ID
 
-	s := id.EncodeToString()
+	var m refs.OwnerID
+	m.SetValue(b[:])
+	require.NoError(t, id.ReadFromV2(m))
 
-	_, err := base58.Decode(s)
-	require.NoError(t, err)
+	require.Equal(t, s, id.EncodeToString())
+}
 
-	var id2 user.ID
+func TestID_DecodeString(t *testing.T) {
+	const s = "NXemr2z9qwACUgnCarY7iYF7uLEePVKBtA"
+	b := [user.IDSize]byte{53, 128, 193, 205, 198, 129, 137, 93, 70, 128, 33, 126, 194, 179, 75, 94, 138, 202, 178, 249, 166, 25, 204, 50, 15}
+	var id user.ID
+	require.NoError(t, id.DecodeString(s))
+	require.Equal(t, b[:], id.WalletBytes())
 
-	err = id2.DecodeString(s)
-	require.NoError(t, err)
-
-	require.Equal(t, id, id2)
-
-	err = id2.DecodeString("_") // any invalid bas58 string
-	require.Error(t, err)
+	t.Run("invalid", func(t *testing.T) {
+		for _, tc := range []struct {
+			name     string
+			str      string
+			contains bool
+			err      string
+		}{
+			{name: "base58", str: "NXemr2z9qwACUgnCarY7iYF7uLEePVKBt_", contains: true, err: "decode base58"},
+			{name: "undersize", str: "zqerPAhxBxcTC7sWuY6D7x6PNSYKifPx", err: "invalid length 24, expected 25"},
+			{name: "oversize", str: "5sgJAVddETxrF3w7Eh2KRtrHEUMB8wXMZtkf", err: "invalid length 26, expected 25"},
+			{name: "prefix", str: "ToY9yFMFk3usAd7zc1gkb5PGYMeDekjkXH", err: "invalid prefix byte 0x42, expected 0x35"},
+			{name: "checksum", str: "NRMge8AquoXsj534yXqs3miJwNn87JsXJP", err: "checksum mismatch"},
+		} {
+			t.Run(tc.name, func(t *testing.T) {
+				err := new(user.ID).DecodeString(tc.str)
+				if tc.contains {
+					require.ErrorContains(t, err, tc.err, tc)
+				} else {
+					require.EqualError(t, err, tc.err, tc)
+				}
+			})
+		}
+	})
 }
 
 func TestID_Equal(t *testing.T) {
