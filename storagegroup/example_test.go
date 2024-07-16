8d71a34c4bb59b3daeed652733882ff9223c429a
Leonard Lyubich <leonard@morphbits.io>
2024-07-16 13:52:06 +0400

    storagegroup: Drop redundant example test
    Similar to 091a279ff2af02d41218b256bef527609ffd9ed7.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- storagegroup/example_test.go
+++ storagegroup/example_test.go
@@ -1,7 +1,6 @@
 package storagegroup_test
 
 import (
-	apiGoStoragegroup "github.com/nspcc-dev/neofs-api-go/v2/storagegroup"
 	"github.com/nspcc-dev/neofs-sdk-go/storagegroup"
 )
 
@@ -15,20 +14,4 @@ func ExampleStorageGroup_validation() {
 	sg.Members()            // objects in the group
 	sg.ValidationDataHash() // hash for objects validation
 	sg.ValidationDataSize() // total objects' payload size
-}
-
-// Instances can be also used to process NeoFS API V2 protocol messages with [https://github.com/nspcc-dev/neofs-api] package.
-func ExampleStorageGroup_marshalling() {
-	// import apiGoStoragegroup "github.com/nspcc-dev/neofs-api-go/v2/storagegroup"
-
-	// On the client side.
-
-	var sg storagegroup.StorageGroup
-	var msg apiGoStoragegroup.StorageGroup
-	sg.WriteToV2(&msg)
-	// *send message*
-
-	// On the server side.
-
-	_ = sg.ReadFromV2(msg)
 }
