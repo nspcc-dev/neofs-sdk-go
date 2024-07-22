e490cd9f93d99f2942f2b00f884135bd6d1336c8
Leonard Lyubich <leonard@morphbits.io>
2024-07-22 11:08:02 +0400

    eacl/test: Add more randomizers and randomness to existing ones
    Also test the functions.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- eacl/test/generate.go
+++ eacl/test/generate.go
@@ -2,6 +2,7 @@ package eacltest
 
 import (
 	"math/rand"
+	"strconv"
 
 	cidtest "github.com/nspcc-dev/neofs-sdk-go/container/id/test"
 	"github.com/nspcc-dev/neofs-sdk-go/eacl"
@@ -16,14 +17,48 @@ func Target() eacl.Target {
 	return eacl.NewTargetByAccounts(usertest.IDs(1 + rand.Intn(10)))
 }
 
-// Record returns random eacl.Record.
-func Record() eacl.Record {
-	return eacl.ConstructRecord(eacl.ActionAllow, eacl.OperationRangeHash, []eacl.Target{Target(), Target()},
-		eacl.NewFilterObjectsFromContainer(cidtest.ID()),
-		eacl.NewFilterObjectOwnerEquals(usertest.ID()),
-	)
+// Targets returns n random eacl.Target instances.
+func Targets(n int) []eacl.Target {
+	res := make([]eacl.Target, n)
+	for i := range res {
+		res[i] = Target()
+	}
+	return res
 }
 
+// Filter returns random eacl.Filter.
+func Filter() eacl.Filter {
+	si := strconv.Itoa(rand.Int())
+	return eacl.ConstructFilter(eacl.FilterHeaderType(rand.Uint32()), "key_"+si, eacl.Match(rand.Uint32()), "val_"+si)
+}
+
+// Filters returns n random eacl.Filter instances.
+func Filters(n int) []eacl.Filter {
+	res := make([]eacl.Filter, n)
+	for i := range res {
+		res[i] = Filter()
+	}
+	return res
+}
+
+// Record returns random eacl.Record.
+func Record() eacl.Record {
+	tn := 1 + rand.Intn(10)
+	fn := 1 + rand.Intn(10)
+	return eacl.ConstructRecord(eacl.Action(rand.Uint32()), eacl.Operation(rand.Uint32()), Targets(tn), Filters(fn)...)
+}
+
+// Records returns n random eacl.Record instances.
+func Records(n int) []eacl.Record {
+	res := make([]eacl.Record, n)
+	for i := range res {
+		res[i] = Record()
+	}
+	return res
+}
+
+// Table returns random eacl.Table.
 func Table() eacl.Table {
-	return eacl.NewTableForContainer(cidtest.ID(), []eacl.Record{Record(), Record()})
+	n := 1 + rand.Intn(10)
+	return eacl.NewTableForContainer(cidtest.ID(), Records(n))
 }
