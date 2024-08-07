61ccde983063ec5f60742f15d4122fd3f2731331
Leonard Lyubich <leonard@morphbits.io>
2024-08-07 15:06:06 +0400

    eacl: Rework consistent string mapping of enumerations
    Enum types provide `EncodeToString`/`DecodeString` interface
    representing stable stringers. Since the string representation is not
    declared by the NeoFS protocol, applications are allowed to use this
    functionality only for internal conversions (e.g. parsing command line
    input). In addition, enumeration types are simple integers. Thus,
    implementing this functionality as type methods is quite redundant. At
    the same time, exporting consistent in-box mapping can simplify
    development of some apps.
    
    `XToString` / `XFromString` function are provided now. Being global
    functions they emphasize that if the app is satisfied with the lib
    strings - it can take and use them, otherwise reimplement them by
    analogy. Methods are deprecated for the sake of new functions.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- eacl/enums.go
+++ eacl/enums.go
@@ -110,39 +110,78 @@ func ActionFromV2(action v2acl.Action) (a Action) {
 	return a
 }
 
+const (
+	actionStringZero  = "ACTION_UNSPECIFIED"
+	actionStringAllow = "ALLOW"
+	actionStringDeny  = "DENY"
+)
+
+// ActionToString maps Action values to strings:
+//   - 0: ACTION_UNSPECIFIED
+//   - [ActionAllow]: ALLOW
+//   - [ActionDeny]: DENY
+//
+// All other values are 'ACTION_UNSPECIFIED'.
+//
+// The mapping is consistent and resilient to lib updates. At the same time,
+// please note that this is not a NeoFS protocol format. Use [Action.String] to
+// get any human-readable text for printing.
+func ActionToString(a Action) string {
+	switch a {
+	default:
+		return actionStringZero
+	case ActionAllow:
+		return actionStringAllow
+	case ActionDeny:
+		return actionStringDeny
+	}
+}
+
+// ActionFromString maps strings to Action values in reverse to
+// [ActionToString]. Returns false if s is incorrect or unknown.
+func ActionFromString(s string) (Action, bool) {
+	switch s {
+	default:
+		return 0, false
+	case "ACTION_UNSPECIFIED":
+		return 0, true
+	case "ALLOW":
+		return ActionAllow, true
+	case "DENY":
+		return ActionDeny, true
+	}
+}
+
 // EncodeToString returns string representation of Action.
 //
 // String mapping:
 //   - ActionAllow: ALLOW;
 //   - ActionDeny: DENY;
 //   - ActionUnspecified, default: ACTION_UNSPECIFIED.
-func (a Action) EncodeToString() string {
-	return a.ToV2().String()
-}
+//
+// Deprecated: use [ActionToString] instead.
+func (a Action) EncodeToString() string { return ActionToString(a) }
 
 // String implements fmt.Stringer.
 //
 // String is designed to be human-readable, and its format MAY differ between
-// SDK versions. String MAY return same result as EncodeToString. String MUST NOT
-// be used to encode ID into NeoFS protocol string.
+// SDK versions. Use [ActionToString] and [ActionFromString] for consistent
+// mapping.
 func (a Action) String() string {
-	return a.EncodeToString()
+	return ActionToString(a)
 }
 
 // DecodeString parses Action from a string representation.
 // It is a reverse action to EncodeToString().
 //
 // Returns true if s was parsed successfully.
+// Deprecated: use [ActionFromString] instead.
 func (a *Action) DecodeString(s string) bool {
-	var g v2acl.Action
-
-	ok := g.FromString(s)
-
-	if ok {
-		*a = ActionFromV2(g)
+	if v, ok := ActionFromString(s); ok {
+		*a = v
+		return true
 	}
-
-	return ok
+	return false
 }
 
 // ToV2 converts Operation to v2 Operation enum value.
@@ -191,6 +230,78 @@ func OperationFromV2(operation v2acl.Operation) (o Operation) {
 	return o
 }
 
+const (
+	opStringZero      = "OPERATION_UNSPECIFIED"
+	opStringGet       = "GET"
+	opStringHead      = "HEAD"
+	opStringPut       = "PUT"
+	opStringDelete    = "DELETE"
+	opStringSearch    = "SEARCH"
+	opStringRange     = "GETRANGE"
+	opStringRangeHash = "GETRANGEHASH"
+)
+
+// OperationToString maps Operation values to strings:
+//   - 0: OPERATION_UNSPECIFIED
+//   - [OperationGet]: GET
+//   - [OperationHead]: HEAD
+//   - [OperationPut]: PUT
+//   - [OperationDelete]: DELETE
+//   - [OperationSearch]: SEARCH
+//   - [OperationRange]: GETRANGE
+//   - [OperationRangeHash]: GETRANGEHASH
+//
+// All other values are 'OPERATION_UNSPECIFIED'.
+//
+// The mapping is consistent and resilient to lib updates. At the same time,
+// please note that this is not a NeoFS protocol format. Use [Operation.String] to
+// get any human-readable text for printing.
+func OperationToString(op Operation) string {
+	switch op {
+	default:
+		return opStringZero
+	case OperationGet:
+		return opStringGet
+	case OperationHead:
+		return opStringHead
+	case OperationPut:
+		return opStringPut
+	case OperationDelete:
+		return opStringDelete
+	case OperationSearch:
+		return opStringSearch
+	case OperationRange:
+		return opStringRange
+	case OperationRangeHash:
+		return opStringRangeHash
+	}
+}
+
+// OperationFromString maps strings to Operation values in reverse to
+// [OperationToString]. Returns false if s is incorrect or unknown.
+func OperationFromString(s string) (Operation, bool) {
+	switch s {
+	default:
+		return 0, false
+	case "OPERATION_UNSPECIFIED":
+		return 0, true
+	case opStringGet:
+		return OperationGet, true
+	case opStringHead:
+		return OperationHead, true
+	case opStringPut:
+		return OperationPut, true
+	case opStringDelete:
+		return OperationDelete, true
+	case opStringSearch:
+		return OperationSearch, true
+	case opStringRange:
+		return OperationRange, true
+	case opStringRangeHash:
+		return OperationRangeHash, true
+	}
+}
+
 // EncodeToString returns string representation of Operation.
 //
 // String mapping:
@@ -202,33 +313,30 @@ func OperationFromV2(operation v2acl.Operation) (o Operation) {
 //   - OperationRange: GETRANGE;
 //   - OperationRangeHash: GETRANGEHASH;
 //   - OperationUnspecified, default: OPERATION_UNSPECIFIED.
-func (o Operation) EncodeToString() string {
-	return o.ToV2().String()
-}
+//
+// Deprecated: use [OperationToString] instead.
+func (o Operation) EncodeToString() string { return OperationToString(o) }
 
 // String implements fmt.Stringer.
 //
 // String is designed to be human-readable, and its format MAY differ between
-// SDK versions. String MAY return same result as EncodeToString. String MUST NOT
-// be used to encode ID into NeoFS protocol string.
+// SDK versions. Use [OperationToString] and [OperationFromString] for
+// consistent mapping.
 func (o Operation) String() string {
-	return o.EncodeToString()
+	return OperationToString(o)
 }
 
 // DecodeString parses Operation from a string representation.
 // It is a reverse action to EncodeToString().
 //
 // Returns true if s was parsed successfully.
+// Deprecated: use [OperationFromString] instead.
 func (o *Operation) DecodeString(s string) bool {
-	var g v2acl.Operation
-
-	ok := g.FromString(s)
-
-	if ok {
-		*o = OperationFromV2(g)
+	if v, ok := OperationFromString(s); ok {
+		*o = v
+		return true
 	}
-
-	return ok
+	return false
 }
 
 // ToV2 converts Role to v2 Role enum value.
@@ -261,6 +369,54 @@ func RoleFromV2(role v2acl.Role) (r Role) {
 	return r
 }
 
+const (
+	roleStringZero   = "ROLE_UNSPECIFIED"
+	roleStringUser   = "USER"
+	roleStringSystem = "SYSTEM"
+	roleStringOthers = "OTHERS"
+)
+
+// RoleToString maps Role values to strings:
+//   - 0: ROLE_UNSPECIFIED
+//   - [RoleUser]: USER
+//   - [RoleSystem]: SYSTEM
+//   - [RoleOthers]: OTHERS
+//
+// All other values are 'ROLE_UNSPECIFIED'.
+//
+// The mapping is consistent and resilient to lib updates. At the same time,
+// please note that this is not a NeoFS protocol format. Use [Role.String] to
+// get any human-readable text for printing.
+func RoleToString(r Role) string {
+	switch r {
+	default:
+		return roleStringZero
+	case RoleUser:
+		return roleStringUser
+	case RoleSystem:
+		return roleStringSystem
+	case RoleOthers:
+		return roleStringOthers
+	}
+}
+
+// RoleFromString maps strings to Role values in reverse to [RoleToString].
+// Returns false if s is incorrect or unknown.
+func RoleFromString(s string) (Role, bool) {
+	switch s {
+	default:
+		return 0, false
+	case "ROLE_UNSPECIFIED":
+		return 0, true
+	case roleStringUser:
+		return RoleUser, true
+	case roleStringSystem:
+		return RoleSystem, true
+	case roleStringOthers:
+		return RoleOthers, true
+	}
+}
+
 // EncodeToString returns string representation of Role.
 //
 // String mapping:
@@ -268,33 +424,29 @@ func RoleFromV2(role v2acl.Role) (r Role) {
 //   - RoleSystem: SYSTEM;
 //   - RoleOthers: OTHERS;
 //   - RoleUnspecified, default: ROLE_UNKNOWN.
-func (r Role) EncodeToString() string {
-	return r.ToV2().String()
-}
+//
+// Deprecated: use [RoleToString] instead.
+func (r Role) EncodeToString() string { return RoleToString(r) }
 
 // String implements fmt.Stringer.
 //
 // String is designed to be human-readable, and its format MAY differ between
-// SDK versions. String MAY return same result as EncodeToString. String MUST NOT
-// be used to encode ID into NeoFS protocol string.
+// SDK versions. Use [RoleToString] and [RoleFromString] for consistent mapping.
 func (r Role) String() string {
-	return r.EncodeToString()
+	return RoleToString(r)
 }
 
 // DecodeString parses Role from a string representation.
 // It is a reverse action to EncodeToString().
 //
 // Returns true if s was parsed successfully.
+// Deprecated: use [RoleFromString] instead.
 func (r *Role) DecodeString(s string) bool {
-	var g v2acl.Role
-
-	ok := g.FromString(s)
-
-	if ok {
-		*r = RoleFromV2(g)
+	if v, ok := RoleFromString(s); ok {
+		*r = v
+		return true
 	}
-
-	return ok
+	return false
 }
 
 // ToV2 converts Match to v2 MatchType enum value.
@@ -331,6 +483,78 @@ func MatchFromV2(match v2acl.MatchType) Match {
 	}
 }
 
+const (
+	matcherStringZero       = "MATCH_TYPE_UNSPECIFIED"
+	matcherStringEqual      = "STRING_EQUAL"
+	matcherStringNotEqual   = "STRING_NOT_EQUAL"
+	matcherStringNotPresent = "NOT_PRESENT"
+	matcherStringNumGT      = "NUM_GT"
+	matcherStringNumGE      = "NUM_GE"
+	matcherStringNumLT      = "NUM_LT"
+	matcherStringNumLE      = "NUM_LE"
+)
+
+// MatcherToString maps Match values to strings:
+//   - 0: MATCH_TYPE_UNSPECIFIED
+//   - [MatchStringEqual]: STRING_EQUAL
+//   - [MatchStringNotEqual]: STRING_NOT_EQUAL
+//   - [MatchNotPresent]: NOT_PRESENT
+//   - [MatchNumGT]: NUM_GT
+//   - [MatchNumGE]: NUM_GE
+//   - [MatchNumLT]: NUM_LT
+//   - [MatchNumLE]: NUM_LE
+//
+// All other values are 'MATCH_TYPE_UNSPECIFIED'.
+//
+// The mapping is consistent and resilient to lib updates. At the same time,
+// please note that this is not a NeoFS protocol format. Use [Match.String] to
+// get any human-readable text for printing.
+func MatcherToString(m Match) string {
+	switch m {
+	default:
+		return matcherStringZero
+	case MatchStringEqual:
+		return matcherStringEqual
+	case MatchStringNotEqual:
+		return matcherStringNotEqual
+	case MatchNotPresent:
+		return matcherStringNotPresent
+	case MatchNumGT:
+		return matcherStringNumGT
+	case MatchNumGE:
+		return matcherStringNumGE
+	case MatchNumLT:
+		return matcherStringNumLT
+	case MatchNumLE:
+		return matcherStringNumLE
+	}
+}
+
+// MatcherFromString maps strings to Match values in reverse to [MatcherToString].
+// Returns false if s is incorrect or unknown.
+func MatcherFromString(s string) (Match, bool) {
+	switch s {
+	default:
+		return 0, false
+	case "MATCH_TYPE_UNSPECIFIED":
+		return 0, true
+	case matcherStringEqual:
+		return MatchStringEqual, true
+	case matcherStringNotEqual:
+		return MatchStringNotEqual, true
+	case matcherStringNotPresent:
+		return MatchNotPresent, true
+	case matcherStringNumGT:
+		return MatchNumGT, true
+	case matcherStringNumGE:
+		return MatchNumGE, true
+	case matcherStringNumLT:
+		return MatchNumLT, true
+	case matcherStringNumLE:
+		return MatchNumLE, true
+	}
+}
+
 // EncodeToString returns string representation of Match.
 //
 // String mapping:
@@ -342,33 +566,30 @@ func MatchFromV2(match v2acl.MatchType) Match {
 //   - MatchNumLT: NUM_LT;
 //   - MatchNumLE: NUM_LE;
 //   - MatchUnspecified, default: MATCH_TYPE_UNSPECIFIED.
-func (m Match) EncodeToString() string {
-	return m.ToV2().String()
-}
+//
+// Deprecated: use [MatcherToString] instead.
+func (m Match) EncodeToString() string { return MatcherToString(m) }
 
 // String implements fmt.Stringer.
 //
 // String is designed to be human-readable, and its format MAY differ between
-// SDK versions. String MAY return same result as EncodeToString. String MUST NOT
-// be used to encode ID into NeoFS protocol string.
+// SDK versions. Use [MatcherToString] and [MatcherFromString] for consistent
+// mapping.
 func (m Match) String() string {
-	return m.EncodeToString()
+	return MatcherToString(m)
 }
 
 // DecodeString parses Match from a string representation.
 // It is a reverse action to EncodeToString().
 //
 // Returns true if s was parsed successfully.
+// Deprecated: use [MatcherFromString] instead.
 func (m *Match) DecodeString(s string) bool {
-	var g v2acl.MatchType
-
-	ok := g.FromString(s)
-
-	if ok {
-		*m = MatchFromV2(g)
+	if v, ok := MatcherFromString(s); ok {
+		*m = v
+		return true
 	}
-
-	return ok
+	return false
 }
 
 // ToV2 converts FilterHeaderType to v2 HeaderType enum value.
@@ -401,37 +622,82 @@ func FilterHeaderTypeFromV2(header v2acl.HeaderType) (h FilterHeaderType) {
 	return h
 }
 
+const (
+	headerTypeStringZero    = "HEADER_UNSPECIFIED"
+	headerTypeStringRequest = "REQUEST"
+	headerTypeStringObject  = "OBJECT"
+	headerTypeStringService = "SERVICE"
+)
+
+// HeaderTypeToString maps FilterHeaderType values to strings:
+//   - 0: HEADER_UNSPECIFIED
+//   - [HeaderFromRequest]: REQUEST
+//   - [HeaderFromObject]: OBJECT
+//   - [HeaderFromService]: SERVICE
+//
+// All other values are 'HEADER_UNSPECIFIED'.
+//
+// The mapping is consistent and resilient to lib updates. At the same time,
+// please note that this is not a NeoFS protocol format. Use
+// [FilterHeaderType.String] to get any human-readable text for printing.
+func HeaderTypeToString(h FilterHeaderType) string {
+	switch h {
+	default:
+		return headerTypeStringZero
+	case HeaderFromRequest:
+		return headerTypeStringRequest
+	case HeaderFromObject:
+		return headerTypeStringObject
+	case HeaderFromService:
+		return headerTypeStringService
+	}
+}
+
+// HeaderTypeFromString maps strings to FilterHeaderType values in reverse to [MatcherToString].
+// Returns false if s is incorrect or unknown.
+func HeaderTypeFromString(s string) (FilterHeaderType, bool) {
+	switch s {
+	default:
+		return 0, false
+	case "HEADER_UNSPECIFIED":
+		return 0, true
+	case headerTypeStringRequest:
+		return HeaderFromRequest, true
+	case headerTypeStringObject:
+		return HeaderFromObject, true
+	case headerTypeStringService:
+		return HeaderFromService, true
+	}
+}
+
 // EncodeToString returns string representation of FilterHeaderType.
 //
 // String mapping:
 //   - HeaderFromRequest: REQUEST;
 //   - HeaderFromObject: OBJECT;
 //   - HeaderTypeUnspecified, default: HEADER_UNSPECIFIED.
-func (h FilterHeaderType) EncodeToString() string {
-	return h.ToV2().String()
-}
+//
+// Deprecated: use [HeaderTypeToString] instead.
+func (h FilterHeaderType) EncodeToString() string { return HeaderTypeToString(h) }
 
 // String implements fmt.Stringer.
 //
 // String is designed to be human-readable, and its format MAY differ between
-// SDK versions. String MAY return same result as EncodeToString. String MUST NOT
-// be used to encode ID into NeoFS protocol string.
+// SDK versions. Use [HeaderTypeToString] and [HeaderTypeFromString] for
+// consistent mapping.
 func (h FilterHeaderType) String() string {
-	return h.EncodeToString()
+	return HeaderTypeToString(h)
 }
 
 // DecodeString parses FilterHeaderType from a string representation.
 // It is a reverse action to EncodeToString().
 //
 // Returns true if s was parsed successfully.
+// Deprecated: use [HeaderTypeFromString] instead.
 func (h *FilterHeaderType) DecodeString(s string) bool {
-	var g v2acl.HeaderType
-
-	ok := g.FromString(s)
-
-	if ok {
-		*h = FilterHeaderTypeFromV2(g)
+	if v, ok := HeaderTypeFromString(s); ok {
+		*h = v
+		return true
 	}
-
-	return ok
+	return false
 }
