b97bc50977ffa70e59d72a6d6843e2ade38e54f0
Leonard Lyubich <leonard@morphbits.io>
2024-09-20 11:22:27 +0400

    accounting: Rework tests for `Decimal`
    Test each method independently and use fixed reference values/binary.
    Also maximize the coverage.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- accounting/decimal_test.go
+++ accounting/decimal_test.go
@@ -9,11 +9,25 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	anyValidValue     = 323508430
+	anyValidPrecision = 895494320
+)
+
+// set by init.
+var validDecimal accounting.Decimal
+
+func init() {
+	validDecimal.SetValue(anyValidValue)
+	validDecimal.SetPrecision(anyValidPrecision)
+}
+
+var validBinDecimal = []byte{8, 206, 177, 161, 154, 1, 16, 176, 209, 128, 171, 3}
+
 func testDecimalField[T uint32 | int64](
 	t *testing.T,
 	get func(accounting.Decimal) T,
 	set func(*accounting.Decimal, T),
-	getAPI func(*apiaccounting.Decimal) T,
 ) {
 	var d accounting.Decimal
 	require.Zero(t, get(d))
@@ -24,42 +38,66 @@ func testDecimalField[T uint32 | int64](
 	valOther := val + 1
 	set(&d, valOther)
 	require.EqualValues(t, valOther, get(d))
-
-	t.Run("encoding", func(t *testing.T) {
-		t.Run("binary", func(t *testing.T) {
-			var src, dst accounting.Decimal
-
-			set(&dst, val)
-			require.NoError(t, dst.Unmarshal(src.Marshal()))
-			require.Zero(t, get(dst))
-
-			set(&src, val)
-			require.NoError(t, dst.Unmarshal(src.Marshal()))
-			require.EqualValues(t, val, get(dst))
-		})
-		t.Run("api", func(t *testing.T) {
-			var src, dst accounting.Decimal
-			var msg apiaccounting.Decimal
-
-			set(&dst, val)
-			src.WriteToV2(&msg)
-			require.Zero(t, getAPI(&msg))
-			require.NoError(t, dst.ReadFromV2(msg))
-			require.Zero(t, get(dst))
-
-			set(&src, val)
-			src.WriteToV2(&msg)
-			require.EqualValues(t, val, getAPI(&msg))
-			require.NoError(t, dst.ReadFromV2(msg))
-			require.EqualValues(t, val, get(dst))
-		})
-	})
 }
 
 func TestDecimal_SetValue(t *testing.T) {
-	testDecimalField(t, accounting.Decimal.Value, (*accounting.Decimal).SetValue, (*apiaccounting.Decimal).GetValue)
+	testDecimalField(t, accounting.Decimal.Value, (*accounting.Decimal).SetValue)
 }
 
 func TestDecimal_SetPrecision(t *testing.T) {
-	testDecimalField(t, accounting.Decimal.Precision, (*accounting.Decimal).SetPrecision, (*apiaccounting.Decimal).GetPrecision)
+	testDecimalField(t, accounting.Decimal.Precision, (*accounting.Decimal).SetPrecision)
+}
+
+func TestDecimal_ReadFromV2(t *testing.T) {
+	var m apiaccounting.Decimal
+	m.SetValue(anyValidValue)
+	m.SetPrecision(anyValidPrecision)
+
+	var val accounting.Decimal
+	require.NoError(t, val.ReadFromV2(m))
+	require.EqualValues(t, anyValidValue, val.Value())
+	require.EqualValues(t, anyValidPrecision, val.Precision())
+}
+
+func TestDecimal_WriteToV2(t *testing.T) {
+	var val accounting.Decimal
+	var m apiaccounting.Decimal
+
+	// zero
+	val.WriteToV2(&m)
+	require.Zero(t, m.GetValue())
+	require.Zero(t, m.GetValue())
+
+	// filled
+	val.SetValue(anyValidValue)
+	val.SetPrecision(anyValidPrecision)
+
+	val.WriteToV2(&m)
+	require.EqualValues(t, anyValidValue, val.Value())
+	require.EqualValues(t, anyValidPrecision, val.Precision())
+}
+
+func TestToken_Marshal(t *testing.T) {
+	require.Equal(t, validBinDecimal, validDecimal.Marshal())
+}
+
+func TestToken_Unmarshal(t *testing.T) {
+	t.Run("invalid", func(t *testing.T) {
+		t.Run("protobuf", func(t *testing.T) {
+			err := new(accounting.Decimal).Unmarshal([]byte("Hello, world!"))
+			require.ErrorContains(t, err, "proto")
+			require.ErrorContains(t, err, "cannot parse invalid wire-format data")
+		})
+	})
+
+	var val accounting.Decimal
+	// zero
+	require.NoError(t, val.Unmarshal(nil))
+	require.Zero(t, val.Value())
+	require.Zero(t, val.Precision())
+
+	// filled
+	err := val.Unmarshal(validBinDecimal)
+	require.NoError(t, err)
+	require.Equal(t, validDecimal, val)
 }
