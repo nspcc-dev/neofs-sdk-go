b20b16221f149cb5f73eadc4bb67fef9566b88f0
Leonard Lyubich <leonard@morphbits.io>
2024-10-25 14:43:06 +0300

    object/test: Assert that generated instance follows the protocol
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

---
+++ object/test/generate_test.go
@@ -0,0 +1,28 @@
+package objecttest_test
+
+import (
+	"testing"
+
+	"github.com/nspcc-dev/neofs-sdk-go/object"
+	objecttest "github.com/nspcc-dev/neofs-sdk-go/object/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestObject(t *testing.T) {
+	obj := objecttest.Object()
+	require.NotEqual(t, obj, objecttest.Object())
+
+	var dst1 object.Object
+	require.NoError(t, dst1.Unmarshal(obj.Marshal()))
+	require.Equal(t, obj, dst1)
+
+	var dst2 object.Object
+	require.NoError(t, dst2.ReadFromV2(*obj.ToV2()))
+	require.Equal(t, obj, dst2)
+
+	j, err := obj.MarshalJSON()
+	require.NoError(t, err)
+	var dst3 object.Object
+	require.NoError(t, dst3.UnmarshalJSON(j))
+	require.Equal(t, obj, dst3)
+}
