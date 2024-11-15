nspcc-dev/neofs-sdk-go

commit ade8822a2f6b082291cbcba3bf3611d4a7cf7e73

[#170] version: Add docs, refactor
Signed-off-by: Alex Vanin <alexey@nspcc.ru>

object/object.go
@@ -122,17 +122,20 @@ func (o *Object) SetPayload(v []byte) {
 
 // Version returns version of the object.
 func (o *Object) Version() *version.Version {
-	return version.NewFromV2(
-		(*object.Object)(o).
-			GetHeader().
-			GetVersion(),
-	)
+	var ver version.Version
+	if verV2 := (*object.Object)(o).GetHeader().GetVersion(); verV2 != nil {
+		ver.ReadFromV2(*verV2)
+	}
+	return &ver
 }
 
 // SetVersion sets version of the object.
 func (o *Object) SetVersion(v *version.Version) {
+	var verV2 refs.Version
+	v.WriteToV2(&verV2)
+
 	o.setHeaderField(func(h *object.Header) {
-		h.SetVersion(v.ToV2())
+		h.SetVersion(&verV2)
 	})
 }
 

audit/result.go
@@ -2,6 +2,7 @@ package audit
 
 import (
 	"github.com/nspcc-dev/neofs-api-go/v2/audit"
+	"github.com/nspcc-dev/neofs-api-go/v2/refs"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
 	"github.com/nspcc-dev/neofs-sdk-go/version"
@@ -28,7 +29,9 @@ type Result struct {
 // See also Unmarshal.
 func (r *Result) Marshal() []byte {
 	if !r.versionEncoded {
-		r.v2.SetVersion(version.Current().ToV2())
+		var verV2 refs.Version
+		version.Current().WriteToV2(&verV2)
+		r.v2.SetVersion(&verV2)
 		r.versionEncoded = true
 	}
 

client/common.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 	"fmt"
 
+	"github.com/nspcc-dev/neofs-api-go/v2/refs"
 	"github.com/nspcc-dev/neofs-api-go/v2/rpc/client"
 	v2session "github.com/nspcc-dev/neofs-api-go/v2/session"
 	"github.com/nspcc-dev/neofs-api-go/v2/signature"
@@ -159,7 +160,9 @@ func (x contextCall) prepareRequest() {
 	}
 
 	if meta.GetVersion() == nil {
-		meta.SetVersion(version.Current().ToV2())
+		var verV2 refs.Version
+		version.Current().WriteToV2(&verV2)
+		meta.SetVersion(&verV2)
 	}
 
 	meta.SetNetworkMagic(x.netMagic)

client/netmap.go
@@ -91,7 +91,11 @@ func (c *Client) EndpointInfo(ctx context.Context, prm PrmEndpointInfo) (*ResEnd
 
 		body := resp.GetBody()
 
-		res.setLatestVersion(version.NewFromV2(body.GetVersion()))
+		var ver version.Version
+		if v2ver := body.GetVersion(); v2ver != nil {
+			ver.ReadFromV2(*v2ver)
+		}
+		res.setLatestVersion(&ver)
 		res.setNodeInfo(netmap.NewNodeInfoFromV2(body.GetNodeInfo()))
 	}
 

container/container.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/nspcc-dev/neofs-api-go/v2/container"
+	"github.com/nspcc-dev/neofs-api-go/v2/refs"
 	"github.com/nspcc-dev/neofs-sdk-go/acl"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
 	"github.com/nspcc-dev/neofs-sdk-go/netmap"
@@ -53,7 +54,8 @@ func New(opts ...Option) *Container {
 	}
 
 	cnr.SetAttributes(cnrOptions.attributes)
-	cnr.SetVersion(version.Current())
+	ver := version.Current()
+	cnr.SetVersion(&ver)
 
 	return cnr
 }
@@ -98,11 +100,17 @@ func CalculateID(c *Container) *cid.ID {
 }
 
 func (c *Container) Version() *version.Version {
-	return version.NewFromV2(c.v2.GetVersion())
+	var ver version.Version
+	if v2ver := c.v2.GetVersion(); v2ver != nil {
+		ver.ReadFromV2(*c.v2.GetVersion())
+	}
+	return &ver
 }
 
 func (c *Container) SetVersion(v *version.Version) {
-	c.v2.SetVersion(v.ToV2())
+	var verV2 refs.Version
+	v.WriteToV2(&verV2)
+	c.v2.SetVersion(&verV2)
 }
 
 func (c *Container) OwnerID() *owner.ID {

container/container_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/nspcc-dev/neofs-api-go/v2/refs"
 	"github.com/nspcc-dev/neofs-sdk-go/acl"
 	"github.com/nspcc-dev/neofs-sdk-go/container"
 	containertest "github.com/nspcc-dev/neofs-sdk-go/container/test"
@@ -34,7 +35,7 @@ func TestNewContainer(t *testing.T) {
 	c.SetOwnerID(ownerID)
 
 	ver := versiontest.Version()
-	c.SetVersion(ver)
+	c.SetVersion(&ver)
 
 	v2 := c.ToV2()
 	newContainer := container.NewContainerFromV2(v2)
@@ -48,7 +49,7 @@ func TestNewContainer(t *testing.T) {
 
 	require.EqualValues(t, newNonce, nonce)
 	require.EqualValues(t, newContainer.OwnerID(), ownerID)
-	require.EqualValues(t, newContainer.Version(), ver)
+	require.EqualValues(t, *newContainer.Version(), ver)
 }
 
 func TestContainerEncoding(t *testing.T) {
@@ -112,7 +113,7 @@ func TestContainer_ToV2(t *testing.T) {
 		require.Nil(t, cnt.OwnerID())
 
 		require.EqualValues(t, acl.PrivateBasicRule, cnt.BasicACL())
-		require.Equal(t, version.Current(), cnt.Version())
+		require.Equal(t, version.Current(), *cnt.Version())
 
 		nonce, err := cnt.NonceUUID()
 		require.NoError(t, err)
@@ -131,6 +132,9 @@ func TestContainer_ToV2(t *testing.T) {
 		require.Nil(t, cntV2.GetOwnerID())
 
 		require.Equal(t, uint32(acl.PrivateBasicRule), cntV2.GetBasicACL())
-		require.Equal(t, version.Current().ToV2(), cntV2.GetVersion())
+
+		var verV2 refs.Version
+		version.Current().WriteToV2(&verV2)
+		require.Equal(t, verV2, *cntV2.GetVersion())
 	})
 }

container/test/generate.go
@@ -26,8 +26,9 @@ func Attributes() container.Attributes {
 // Container returns random container.Container.
 func Container() *container.Container {
 	x := container.New()
+	ver := versiontest.Version()
 
-	x.SetVersion(versiontest.Version())
+	x.SetVersion(&ver)
 	x.SetAttributes(Attributes())
 	x.SetOwnerID(ownertest.ID())
 	x.SetBasicACL(123)

eacl/record_test.go
@@ -170,7 +170,7 @@ func TestReservedRecords(t *testing.T) {
 			value: "bar",
 		},
 		{
-			f:     func(r *Record) { r.AddObjectVersionFilter(MatchStringEqual, v) },
+			f:     func(r *Record) { r.AddObjectVersionFilter(MatchStringEqual, &v) },
 			key:   v2acl.FilterObjectVersion,
 			value: v.String(),
 		},

commit ebb37bc98a3b72cf3ced92dc52b8114ce2fa3e42

object: Fix version return when unset
Some kind of regression from ade8822a2f6b082291cbcba3bf3611d4a7cf7e73:
`Version` method started to return pointer to zero instance instead of
nil. Since the absence of a field and an explicitly set zero are
different cases, the behavior was initially more correct, so this
returns it.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

object/object.go
@@ -350,15 +350,17 @@ func (o *Object) SetPayload(v []byte) {
 	(*object.Object)(o).SetPayload(v)
 }
 
-// Version returns version of the object.
+// Version returns version of the object. Returns nil if the version is unset.
 //
 // See also [Object.SetVersion].
 func (o Object) Version() *version.Version {
+	verV2 := (*object.Object)(&o).GetHeader().GetVersion()
+	if verV2 == nil {
+		return nil
+	}
 	var ver version.Version
-	if verV2 := (*object.Object)(&o).GetHeader().GetVersion(); verV2 != nil {
-		if err := ver.ReadFromV2(*verV2); err != nil {
-			return nil
-		}
+	if err := ver.ReadFromV2(*verV2); err != nil {
+		return nil
 	}
 	return &ver
 }