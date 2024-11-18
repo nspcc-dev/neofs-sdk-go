56b1f1519ef5d28d12607779f4524ceb52e731a7
Leonard Lyubich <leonard@morphbits.io>
2024-11-18 13:49:39 +0300

    client: Increase coverage for `Dial` method
    One of the added tests fails: it is planned to be corrected in the
    future.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- client/client_test.go
+++ client/client_test.go
@@ -29,35 +29,90 @@ func newClient(t *testing.T, server neoFSAPIServer) *Client {
 	return c
 }
 
-func TestClient_DialContext(t *testing.T) {
+func TestClient_Dial(t *testing.T) {
 	var prmInit PrmInit
 
 	c, err := New(prmInit)
 	require.NoError(t, err)
 
-	// try to connect to any host
-	var prm PrmDial
-	prm.SetServerURI("localhost:8080")
+	t.Run("failure", func(t *testing.T) {
+		t.Run("endpoint", func(t *testing.T) {
+			for _, tc := range []struct {
+				name   string
+				s      string
+				assert func(t testing.TB, err error)
+			}{
+				{name: "missing", s: "", assert: func(t testing.TB, err error) {
+					require.ErrorIs(t, c.Dial(PrmDial{}), ErrMissingServer)
+				}},
+				{name: "contains control char", s: "grpc://st1.storage.fs.neo.org:8080" + string(rune(0x7f)), assert: func(t testing.TB, err error) {
+					require.ErrorContains(t, err, "net/url: invalid control character in URL")
+				}},
+				{name: "missing port", s: "grpc://st1.storage.fs.neo.org", assert: func(t testing.TB, err error) {
+					require.ErrorContains(t, err, "dial tcp: address st1.storage.fs.neo.org: missing port in address")
+				}},
+				{name: "invalid port", s: "grpc://st1.storage.fs.neo.org:foo", assert: func(t testing.TB, err error) {
+					require.ErrorContains(t, err, `invalid port ":foo" after host`)
+				}},
+				{name: "unsupported scheme", s: "unknown://st1.storage.fs.neo.org:8080", assert: func(t testing.TB, err error) {
+					require.ErrorContains(t, err, "unsupported scheme: unknown")
+				}},
+				{name: "multiaddr", s: "/ip4/st1.storage.fs.neo.org/tcp/8080", assert: func(t testing.TB, err error) {
+					require.ErrorContains(t, err, "invalid endpoint options")
+				}},
+				{name: "host only", s: "st1.storage.fs.neo.org", assert: func(t testing.TB, err error) {
+					require.ErrorContains(t, err, "dial tcp: address st1.storage.fs.neo.org: missing port in address")
+				}},
+				{name: "invalid port without scheme", s: "st1.storage.fs.neo.org:foo", assert: func(t testing.TB, err error) {
+					require.ErrorContains(t, err, `invalid port ":foo" after host`)
+				}},
+			} {
+				t.Run(tc.name, func(t *testing.T) {
+					var p PrmDial
+					p.SetServerURI(tc.s)
+					tc.assert(t, c.Dial(p))
+				})
+			}
+		})
+		t.Run("dial timeout", func(t *testing.T) {
+			var p PrmDial
+			p.SetServerURI("grpc://localhost:8080")
+			p.SetTimeout(0)
+			require.ErrorIs(t, c.Dial(p), ErrNonPositiveTimeout)
+			p.SetTimeout(-1)
+			require.ErrorIs(t, c.Dial(p), ErrNonPositiveTimeout)
+		})
+		t.Run("stream timeout", func(t *testing.T) {
+			var p PrmDial
+			p.SetServerURI("grpc://localhost:8080")
+			p.SetStreamTimeout(0)
+			require.ErrorIs(t, c.Dial(p), ErrNonPositiveTimeout)
+			p.SetStreamTimeout(-1)
+			require.ErrorIs(t, c.Dial(p), ErrNonPositiveTimeout)
+		})
+		t.Run("context", func(t *testing.T) {
+			var anyValidPrm PrmDial
+			anyValidPrm.SetServerURI("localhost:8080")
+			t.Run("cancelled", func(t *testing.T) {
+				ctx, cancel := context.WithCancel(context.Background())
+				cancel()
 
-	assert := func(ctx context.Context, errExpected error) {
-		// use the particular context
-		prm.SetContext(ctx)
+				p := anyValidPrm
+				p.SetContext(ctx)
+				err := c.Dial(p)
+				require.ErrorIs(t, err, context.Canceled)
+			})
+			t.Run("deadline", func(t *testing.T) {
+				ctx, cancel := context.WithTimeout(context.Background(), 0)
+				cancel()
 
-		// expect particular context error according to Dial docs
-		require.ErrorIs(t, c.Dial(prm), errExpected)
-	}
-
-	// create pre-abandoned context
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-
-	assert(ctx, context.Canceled)
-
-	// create "pre-deadlined" context
-	ctx, cancel = context.WithTimeout(context.Background(), 0)
-	defer cancel()
-
-	assert(ctx, context.DeadlineExceeded)
+				p := anyValidPrm
+				p.SetContext(ctx)
+				err := c.Dial(p)
+				require.ErrorIs(t, err, context.DeadlineExceeded)
+			})
+		})
+	})
 }
 
 type nopPublicKey struct{}
