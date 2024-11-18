b8377f9d5772aec09b1af396ff1c574c39348a16
Leonard Lyubich <leonard@morphbits.io>
2024-11-18 16:28:06 +0300

    internal/util: Drop wrong statement from `ParseURI` func docs
    The function return zeros on multiaddr input.
    
    Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

--- internal/util/net.go
+++ internal/util/net.go
@@ -7,7 +7,7 @@ import (
 )
 
 // ParseURI parses URI and returns a host and a flag indicating that TLS is
-// enabled. If multi-address is provided the argument is returned unchanged.
+// enabled.
 func ParseURI(s string) (string, bool, error) {
 	uri, err := url.ParseRequestURI(s)
 	if err != nil {
