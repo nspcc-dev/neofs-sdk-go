nspcc-dev/neofs-sdk-go

commit b75db12121973c8ed9b516294bd6bc6bd0f29e46

client: Inline gRPC mechanics from neofs-api-go lib
One step closer to github.com/nspcc-dev/neofs-api-go/v2 module
deprecation. Previously, for the `Client`, network connecting, sending a
request and receiving a response was almost completely hidden in the
lib. Now the code is built into the client itself.

The test framework used to be too abstract and far from the production
version. Although the gRPC lib is vendored, it covers most of the
`Client` mechanics. In fact, proper testing requires emulating the
behavior of the client-server communication as realistically as possible.
This reworks the tests by abstracting only the network transport
via in-memory buffer implementation supplied by gRPC lib itself.
From now unit tests should provide protocol-declared RPC handlers
exactly like NeoFS storage nodes does. This automatically includes gRPC
layer to testing, making the client implementation more resilient to
corresponding module updates.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

client/netmap.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	v2netmap "github.com/nspcc-dev/neofs-api-go/v2/netmap"
+	protonetmap "github.com/nspcc-dev/neofs-api-go/v2/netmap/grpc"
 	v2session "github.com/nspcc-dev/neofs-api-go/v2/session"
 	"github.com/nspcc-dev/neofs-sdk-go/netmap"
 	"github.com/nspcc-dev/neofs-sdk-go/stat"
@@ -77,7 +78,15 @@ func (c *Client) EndpointInfo(ctx context.Context, prm PrmEndpointInfo) (*ResEnd
 	cc.meta = prm.prmCommonMeta
 	cc.req = &req
 	cc.call = func() (responseV2, error) {
-		return c.server.getNodeInfo(ctx, req)
+		resp, err := c.netmap.LocalNodeInfo(ctx, req.ToGRPCMessage().(*protonetmap.LocalNodeInfoRequest))
+		if err != nil {
+			return nil, rpcErr(err)
+		}
+		var respV2 v2netmap.LocalNodeInfoResponse
+		if err = respV2.FromGRPCMessage(resp); err != nil {
+			return nil, err
+		}
+		return &respV2, nil
 	}
 	cc.result = func(r responseV2) {
 		resp := r.(*v2netmap.LocalNodeInfoResponse)
@@ -155,7 +164,15 @@ func (c *Client) NetworkInfo(ctx context.Context, prm PrmNetworkInfo) (netmap.Ne
 	cc.meta = prm.prmCommonMeta
 	cc.req = &req
 	cc.call = func() (responseV2, error) {
-		return c.server.getNetworkInfo(ctx, req)
+		resp, err := c.netmap.NetworkInfo(ctx, req.ToGRPCMessage().(*protonetmap.NetworkInfoRequest))
+		if err != nil {
+			return nil, rpcErr(err)
+		}
+		var respV2 v2netmap.NetworkInfoResponse
+		if err = respV2.FromGRPCMessage(resp); err != nil {
+			return nil, err
+		}
+		return &respV2, nil
 	}
 	cc.result = func(r responseV2) {
 		resp := r.(*v2netmap.NetworkInfoResponse)
@@ -221,21 +238,23 @@ func (c *Client) NetMapSnapshot(ctx context.Context, _ PrmNetMapSnapshot) (netma
 		return netmap.NetMap{}, err
 	}
 
-	var resp *v2netmap.SnapshotResponse
-
-	resp, err = c.server.netMapSnapshot(ctx, req)
+	resp, err := c.netmap.NetmapSnapshot(ctx, req.ToGRPCMessage().(*protonetmap.NetmapSnapshotRequest))
 	if err != nil {
+		return netmap.NetMap{}, rpcErr(err)
+	}
+	var respV2 v2netmap.SnapshotResponse
+	if err = respV2.FromGRPCMessage(resp); err != nil {
 		return netmap.NetMap{}, err
 	}
 
 	var res netmap.NetMap
-	if err = c.processResponse(resp); err != nil {
+	if err = c.processResponse(&respV2); err != nil {
 		return netmap.NetMap{}, err
 	}
 
 	const fieldNetMap = "network map"
 
-	netMapV2 := resp.GetBody().NetMap()
+	netMapV2 := respV2.GetBody().NetMap()
 	if netMapV2 == nil {
 		err = newErrMissingResponseField(fieldNetMap)
 		return netmap.NetMap{}, err

client/object_delete.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/nspcc-dev/neofs-api-go/v2/acl"
 	v2object "github.com/nspcc-dev/neofs-api-go/v2/object"
+	protoobject "github.com/nspcc-dev/neofs-api-go/v2/object/grpc"
 	v2refs "github.com/nspcc-dev/neofs-api-go/v2/refs"
 	"github.com/nspcc-dev/neofs-sdk-go/bearer"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
@@ -103,19 +104,23 @@ func (c *Client) ObjectDelete(ctx context.Context, containerID cid.ID, objectID 
 		return oid.ID{}, err
 	}
 
-	resp, err := c.server.deleteObject(ctx, req)
+	resp, err := c.object.Delete(ctx, req.ToGRPCMessage().(*protoobject.DeleteRequest))
 	if err != nil {
+		return oid.ID{}, rpcErr(err)
+	}
+	var respV2 v2object.DeleteResponse
+	if err = respV2.FromGRPCMessage(resp); err != nil {
 		return oid.ID{}, err
 	}
 
 	var res oid.ID
-	if err = c.processResponse(resp); err != nil {
+	if err = c.processResponse(&respV2); err != nil {
 		return oid.ID{}, err
 	}
 
 	const fieldTombstone = "tombstone"
 
-	idTombV2 := resp.GetBody().GetTombstone().GetObjectID()
+	idTombV2 := respV2.GetBody().GetTombstone().GetObjectID()
 	if idTombV2 == nil {
 		err = newErrMissingResponseField(fieldTombstone)
 		return oid.ID{}, err

client/object_get.go
@@ -5,9 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"time"
 
 	"github.com/nspcc-dev/neofs-api-go/v2/acl"
 	v2object "github.com/nspcc-dev/neofs-api-go/v2/object"
+	protoobject "github.com/nspcc-dev/neofs-api-go/v2/object/grpc"
 	v2refs "github.com/nspcc-dev/neofs-api-go/v2/refs"
 	"github.com/nspcc-dev/neofs-sdk-go/bearer"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
@@ -58,6 +60,15 @@ type PrmObjectGet struct {
 	prmObjectRead
 }
 
+// used part of [protoobject.ObjectService_GetClient] simplifying test
+// implementations.
+type getObjectResponseStream interface {
+	// Recv reads next message with the object part from the stream. Recv returns
+	// [io.EOF] after the server sent the last message and gracefully finished the
+	// stream. Any other error means stream abort.
+	Recv() (*protoobject.GetResponse, error)
+}
+
 // PayloadReader is a data stream of the particular NeoFS object. Implements
 // [io.ReadCloser].
 //
@@ -66,8 +77,9 @@ type PrmObjectGet struct {
 type PayloadReader struct {
 	cancelCtxStream context.CancelFunc
 
-	client *Client
-	stream getObjectResponseStream
+	client           *Client
+	stream           getObjectResponseStream
+	singleMsgTimeout time.Duration
 
 	err error
 
@@ -81,20 +93,28 @@ type PayloadReader struct {
 // readHeader reads header of the object. Result means success.
 // Failure reason can be received via Close.
 func (x *PayloadReader) readHeader(dst *object.Object) bool {
-	var resp v2object.GetResponse
-	x.err = x.stream.Read(&resp)
+	var resp *protoobject.GetResponse
+	x.err = dowithTimeout(x.singleMsgTimeout, x.cancelCtxStream, func() error {
+		var err error
+		resp, err = x.stream.Recv()
+		return err
+	})
 	if x.err != nil {
 		return false
 	}
+	var respV2 v2object.GetResponse
+	if x.err = respV2.FromGRPCMessage(resp); x.err != nil {
+		return false
+	}
 
-	x.err = x.client.processResponse(&resp)
+	x.err = x.client.processResponse(&respV2)
 	if x.err != nil {
 		return false
 	}
 
 	var partInit *v2object.GetObjectPartInit
 
-	switch v := resp.GetBody().GetObjectPart().(type) {
+	switch v := respV2.GetBody().GetObjectPart().(type) {
 	default:
 		x.err = fmt.Errorf("unexpected message instead of heading part: %T", v)
 		return false
@@ -133,18 +153,26 @@ func (x *PayloadReader) readChunk(buf []byte) (int, bool) {
 	var lastRead int
 
 	for {
-		var resp v2object.GetResponse
-		x.err = x.stream.Read(&resp)
+		var resp *protoobject.GetResponse
+		x.err = dowithTimeout(x.singleMsgTimeout, x.cancelCtxStream, func() error {
+			var err error
+			resp, err = x.stream.Recv()
+			return err
+		})
+		if x.err != nil {
+			return read, false
+		}
+		var respV2 v2object.GetResponse
+		if x.err = respV2.FromGRPCMessage(resp); x.err != nil {
+			return read, false
+		}
+
+		x.err = x.client.processResponse(&respV2)
 		if x.err != nil {
 			return read, false
 		}
 
-		x.err = x.client.processResponse(&resp)
-		if x.err != nil {
-			return read, false
-		}
-
-		part := resp.GetBody().GetObjectPart()
+		part := respV2.GetBody().GetObjectPart()
 		partChunk, ok := part.(*v2object.GetObjectPartChunk)
 		if !ok {
 			x.err = fmt.Errorf("unexpected message instead of chunk part: %T", part)
@@ -280,7 +308,7 @@ func (c *Client) ObjectGetInit(ctx context.Context, containerID cid.ID, objectID
 
 	ctx, cancel := context.WithCancel(ctx)
 
-	stream, err := c.server.getObject(ctx, req)
+	stream, err := c.object.Get(ctx, req.ToGRPCMessage().(*protoobject.GetRequest))
 	if err != nil {
 		cancel()
 		err = fmt.Errorf("open stream: %w", err)
@@ -290,6 +318,7 @@ func (c *Client) ObjectGetInit(ctx context.Context, containerID cid.ID, objectID
 	var r PayloadReader
 	r.cancelCtxStream = cancel
 	r.stream = stream
+	r.singleMsgTimeout = c.streamTimeout
 	r.client = c
 	r.statisticCallback = func(err error) {
 		c.sendStatistic(stat.MethodObjectGetStream, err)
@@ -366,17 +395,20 @@ func (c *Client) ObjectHead(ctx context.Context, containerID cid.ID, objectID oi
 		return nil, err
 	}
 
-	resp, err := c.server.headObject(ctx, req)
+	resp, err := c.object.Head(ctx, req.ToGRPCMessage().(*protoobject.HeadRequest))
 	if err != nil {
-		err = fmt.Errorf("write request: %w", err)
+		return nil, rpcErr(err)
+	}
+	var respV2 v2object.HeadResponse
+	if err = respV2.FromGRPCMessage(resp); err != nil {
 		return nil, err
 	}
 
-	if err = c.processResponse(resp); err != nil {
+	if err = c.processResponse(&respV2); err != nil {
 		return nil, err
 	}
 
-	switch v := resp.GetBody().GetHeaderPart().(type) {
+	switch v := respV2.GetBody().GetHeaderPart().(type) {
 	default:
 		err = fmt.Errorf("unexpected header type %T", v)
 		return nil, err
@@ -405,6 +437,15 @@ type PrmObjectRange struct {
 	prmObjectRead
 }
 
+// used part of [protoobject.ObjectService_GetRangeClient] simplifying test
+// implementations.
+type getObjectPayloadRangeResponseStream interface {
+	// Recv reads next message with the object payload part from the stream. Recv
+	// returns [io.EOF] after the server sent the last message and gracefully
+	// finished the stream. Any other error means stream abort.
+	Recv() (*protoobject.GetRangeResponse, error)
+}
+
 // ObjectRangeReader is designed to read payload range of one object
 // from NeoFS system. Implements [io.ReadCloser].
 //
@@ -417,7 +458,8 @@ type ObjectRangeReader struct {
 
 	err error
 
-	stream getObjectPayloadRangeResponseStream
+	stream           getObjectPayloadRangeResponseStream
+	singleMsgTimeout time.Duration
 
 	tailPayload []byte
 
@@ -443,19 +485,27 @@ func (x *ObjectRangeReader) readChunk(buf []byte) (int, bool) {
 	var lastRead int
 
 	for {
-		var resp v2object.GetRangeResponse
-		x.err = x.stream.Read(&resp)
+		var resp *protoobject.GetRangeResponse
+		x.err = dowithTimeout(x.singleMsgTimeout, x.cancelCtxStream, func() error {
+			var err error
+			resp, err = x.stream.Recv()
+			return err
+		})
 		if x.err != nil {
 			return read, false
 		}
+		var respV2 v2object.GetRangeResponse
+		if x.err = respV2.FromGRPCMessage(resp); x.err != nil {
+			return read, false
+		}
 
-		x.err = x.client.processResponse(&resp)
+		x.err = x.client.processResponse(&respV2)
 		if x.err != nil {
 			return read, false
 		}
 
 		// get chunk message
-		switch v := resp.GetBody().GetRangePart().(type) {
+		switch v := respV2.GetBody().GetRangePart().(type) {
 		default:
 			x.err = fmt.Errorf("unexpected message received: %T", v)
 			return read, false
@@ -615,7 +665,7 @@ func (c *Client) ObjectRangeInit(ctx context.Context, containerID cid.ID, object
 
 	ctx, cancel := context.WithCancel(ctx)
 
-	stream, err := c.server.getObjectPayloadRange(ctx, req)
+	stream, err := c.object.GetRange(ctx, req.ToGRPCMessage().(*protoobject.GetRangeRequest))
 	if err != nil {
 		cancel()
 		err = fmt.Errorf("open stream: %w", err)
@@ -626,6 +676,7 @@ func (c *Client) ObjectRangeInit(ctx context.Context, containerID cid.ID, object
 	r.remainingPayloadLen = int(length)
 	r.cancelCtxStream = cancel
 	r.stream = stream
+	r.singleMsgTimeout = c.streamTimeout
 	r.client = c
 	r.statisticCallback = func(err error) {
 		c.sendStatistic(stat.MethodObjectRangeStream, err)()

client/accounting.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	v2accounting "github.com/nspcc-dev/neofs-api-go/v2/accounting"
+	protoaccounting "github.com/nspcc-dev/neofs-api-go/v2/accounting/grpc"
 	"github.com/nspcc-dev/neofs-api-go/v2/refs"
 	"github.com/nspcc-dev/neofs-sdk-go/accounting"
 	"github.com/nspcc-dev/neofs-sdk-go/stat"
@@ -67,7 +68,15 @@ func (c *Client) BalanceGet(ctx context.Context, prm PrmBalanceGet) (accounting.
 	cc.meta = prm.prmCommonMeta
 	cc.req = &req
 	cc.call = func() (responseV2, error) {
-		return c.server.getBalance(ctx, req)
+		resp, err := c.accounting.Balance(ctx, req.ToGRPCMessage().(*protoaccounting.BalanceRequest))
+		if err != nil {
+			return nil, rpcErr(err)
+		}
+		var respV2 v2accounting.BalanceResponse
+		if err = respV2.FromGRPCMessage(resp); err != nil {
+			return nil, err
+		}
+		return &respV2, nil
 	}
 	cc.result = func(r responseV2) {
 		resp := r.(*v2accounting.BalanceResponse)

client/accounting_test.go
@@ -5,30 +5,41 @@ import (
 	"fmt"
 	"testing"
 
-	apiaccounting "github.com/nspcc-dev/neofs-api-go/v2/accounting"
+	v2accounting "github.com/nspcc-dev/neofs-api-go/v2/accounting"
+	protoaccounting "github.com/nspcc-dev/neofs-api-go/v2/accounting/grpc"
 	neofscryptotest "github.com/nspcc-dev/neofs-sdk-go/crypto/test"
 	"github.com/stretchr/testify/require"
 )
 
-type testGetBalanceServer struct {
-	unimplementedNeoFSAPIServer
+// returns Client of Accounting service provided by given server.
+func newTestAccountingClient(t testing.TB, srv protoaccounting.AccountingServiceServer) *Client {
+	return newClient(t, testService{desc: &protoaccounting.AccountingService_ServiceDesc, impl: srv})
 }
 
-func (x testGetBalanceServer) getBalance(context.Context, apiaccounting.BalanceRequest) (*apiaccounting.BalanceResponse, error) {
-	var body apiaccounting.BalanceResponseBody
-	body.SetBalance(new(apiaccounting.Decimal))
-	var resp apiaccounting.BalanceResponse
-	resp.SetBody(&body)
+type testGetBalanceServer struct {
+	protoaccounting.UnimplementedAccountingServiceServer
+}
 
-	if err := signServiceMessage(neofscryptotest.Signer(), &resp, nil); err != nil {
+func (x *testGetBalanceServer) Balance(context.Context, *protoaccounting.BalanceRequest) (*protoaccounting.BalanceResponse, error) {
+	resp := protoaccounting.BalanceResponse{
+		Body: &protoaccounting.BalanceResponse_Body{
+			Balance: new(protoaccounting.Decimal),
+		},
+	}
+
+	var respV2 v2accounting.BalanceResponse
+	if err := respV2.FromGRPCMessage(&resp); err != nil {
+		panic(err)
+	}
+	if err := signServiceMessage(neofscryptotest.Signer(), &respV2, nil); err != nil {
 		return nil, fmt.Errorf("sign response message: %w", err)
 	}
 
-	return &resp, nil
+	return respV2.ToGRPCMessage().(*protoaccounting.BalanceResponse), nil
 }
 
 func TestClient_BalanceGet(t *testing.T) {
-	c := newClient(t, nil)
+	c := newClient(t)
 	ctx := context.Background()
 
 	t.Run("missing", func(t *testing.T) {

client/api.go
@@ -1,304 +1,10 @@
 package client
 
 import (
-	"context"
 	"fmt"
-
-	"github.com/nspcc-dev/neofs-api-go/v2/accounting"
-	"github.com/nspcc-dev/neofs-api-go/v2/container"
-	"github.com/nspcc-dev/neofs-api-go/v2/netmap"
-	"github.com/nspcc-dev/neofs-api-go/v2/object"
-	"github.com/nspcc-dev/neofs-api-go/v2/reputation"
-	rpcapi "github.com/nspcc-dev/neofs-api-go/v2/rpc"
-	"github.com/nspcc-dev/neofs-api-go/v2/rpc/client"
-	"github.com/nspcc-dev/neofs-api-go/v2/rpc/common"
-	"github.com/nspcc-dev/neofs-api-go/v2/session"
 )
-
-type getObjectResponseStream interface {
-	Read(*object.GetResponse) error
-}
-
-type getObjectPayloadRangeResponseStream interface {
-	Read(*object.GetRangeResponse) error
-}
-
-type searchObjectsResponseStream interface {
-	Read(*object.SearchResponse) error
-}
-
-// interface of NeoFS API server. Exists for test purposes only.
-type neoFSAPIServer interface {
-	createSession(context.Context, session.CreateRequest) (*session.CreateResponse, error)
-	getBalance(context.Context, accounting.BalanceRequest) (*accounting.BalanceResponse, error)
-	netMapSnapshot(context.Context, netmap.SnapshotRequest) (*netmap.SnapshotResponse, error)
-	getNetworkInfo(context.Context, netmap.NetworkInfoRequest) (*netmap.NetworkInfoResponse, error)
-	getNodeInfo(context.Context, netmap.LocalNodeInfoRequest) (*netmap.LocalNodeInfoResponse, error)
-	putContainer(context.Context, container.PutRequest) (*container.PutResponse, error)
-	getContainer(context.Context, container.GetRequest) (*container.GetResponse, error)
-	deleteContainer(context.Context, container.DeleteRequest) (*container.DeleteResponse, error)
-	listContainers(context.Context, container.ListRequest) (*container.ListResponse, error)
-	getEACL(context.Context, container.GetExtendedACLRequest) (*container.GetExtendedACLResponse, error)
-	setEACL(context.Context, container.SetExtendedACLRequest) (*container.SetExtendedACLResponse, error)
-	announceContainerSpace(context.Context, container.AnnounceUsedSpaceRequest) (*container.AnnounceUsedSpaceResponse, error)
-	announceIntermediateReputation(context.Context, reputation.AnnounceIntermediateResultRequest) (*reputation.AnnounceIntermediateResultResponse, error)
-	announceLocalTrust(context.Context, reputation.AnnounceLocalTrustRequest) (*reputation.AnnounceLocalTrustResponse, error)
-	putObject(context.Context) (objectWriter, error)
-	deleteObject(context.Context, object.DeleteRequest) (*object.DeleteResponse, error)
-	hashObjectPayloadRanges(context.Context, object.GetRangeHashRequest) (*object.GetRangeHashResponse, error)
-	headObject(context.Context, object.HeadRequest) (*object.HeadResponse, error)
-	getObject(context.Context, object.GetRequest) (getObjectResponseStream, error)
-	getObjectPayloadRange(context.Context, object.GetRangeRequest) (getObjectPayloadRangeResponseStream, error)
-	searchObjects(context.Context, object.SearchRequest) (searchObjectsResponseStream, error)
-}
-
-// wrapper over real client connection which communicates over NeoFS API protocol.
-// Provides neoFSAPIServer for Client instances used in real applications.
-type coreServer client.Client
 
 // unifies errors of all RPC.
 func rpcErr(e error) error {
 	return fmt.Errorf("rpc failure: %w", e)
-}
-
-// executes NetmapService.NetmapSnapshot RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) netMapSnapshot(ctx context.Context, req netmap.SnapshotRequest) (*netmap.SnapshotResponse, error) {
-	resp, err := rpcapi.NetMapSnapshot((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-
-	return resp, nil
-}
-
-// executes NetmapService.NetworkInfo RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) getNetworkInfo(ctx context.Context, req netmap.NetworkInfoRequest) (*netmap.NetworkInfoResponse, error) {
-	resp, err := rpcapi.NetworkInfo((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes NetmapService.LocalNodeInfo RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) getNodeInfo(ctx context.Context, req netmap.LocalNodeInfoRequest) (*netmap.LocalNodeInfoResponse, error) {
-	resp, err := rpcapi.LocalNodeInfo((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes AccountingService.Balance RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) getBalance(ctx context.Context, req accounting.BalanceRequest) (*accounting.BalanceResponse, error) {
-	resp, err := rpcapi.Balance((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes SessionService.Create RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) createSession(ctx context.Context, req session.CreateRequest) (*session.CreateResponse, error) {
-	resp, err := rpcapi.CreateSession((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-
-	return resp, nil
-}
-
-// executes ContainerService.Put RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) putContainer(ctx context.Context, req container.PutRequest) (*container.PutResponse, error) {
-	resp, err := rpcapi.PutContainer((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes ContainerService.Get RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) getContainer(ctx context.Context, req container.GetRequest) (*container.GetResponse, error) {
-	resp, err := rpcapi.GetContainer((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes ContainerService.Delete RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) deleteContainer(ctx context.Context, req container.DeleteRequest) (*container.DeleteResponse, error) {
-	// rpcapi.DeleteContainer returns wrong response type
-	resp := new(container.DeleteResponse)
-	err := client.SendUnary((*client.Client)(x),
-		common.CallMethodInfoUnary("neo.fs.v2.container.ContainerService", "Delete"),
-		&req, resp, client.WithContext(ctx))
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
-}
-
-// executes ContainerService.List RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) listContainers(ctx context.Context, req container.ListRequest) (*container.ListResponse, error) {
-	resp, err := rpcapi.ListContainers((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes ContainerService.GetExtendedACL RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) getEACL(ctx context.Context, req container.GetExtendedACLRequest) (*container.GetExtendedACLResponse, error) {
-	resp, err := rpcapi.GetEACL((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes ContainerService.SetExtendedACL RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) setEACL(ctx context.Context, req container.SetExtendedACLRequest) (*container.SetExtendedACLResponse, error) {
-	// rpcapi.SetEACL returns wrong response type
-	resp := new(container.SetExtendedACLResponse)
-	err := client.SendUnary((*client.Client)(x),
-		common.CallMethodInfoUnary("neo.fs.v2.container.ContainerService", "SetExtendedACL"),
-		&req, resp, client.WithContext(ctx))
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
-}
-
-// executes ContainerService.AnnounceUsedSpace RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) announceContainerSpace(ctx context.Context, req container.AnnounceUsedSpaceRequest) (*container.AnnounceUsedSpaceResponse, error) {
-	// rpcapi.AnnounceUsedSpace returns wrong response type
-	resp := new(container.AnnounceUsedSpaceResponse)
-	err := client.SendUnary((*client.Client)(x),
-		common.CallMethodInfoUnary("neo.fs.v2.container.ContainerService", "AnnounceUsedSpace"),
-		&req, resp, client.WithContext(ctx))
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
-}
-
-// executes ReputationService.AnnounceIntermediateResult RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) announceIntermediateReputation(ctx context.Context, req reputation.AnnounceIntermediateResultRequest) (*reputation.AnnounceIntermediateResultResponse, error) {
-	resp, err := rpcapi.AnnounceIntermediateResult((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes ReputationService.AnnounceLocalTrust RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) announceLocalTrust(ctx context.Context, req reputation.AnnounceLocalTrustRequest) (*reputation.AnnounceLocalTrustResponse, error) {
-	resp, err := rpcapi.AnnounceLocalTrust((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-type corePutObjectStream struct {
-	reqWriter *rpcapi.PutRequestWriter
-	resp      object.PutResponse
-}
-
-func (x *corePutObjectStream) Write(req *object.PutRequest) error {
-	return x.reqWriter.Write(req)
-}
-
-func (x *corePutObjectStream) Close() (*object.PutResponse, error) {
-	if err := x.reqWriter.Close(); err != nil {
-		return nil, err
-	}
-	return &x.resp, nil
-}
-
-// executes ObjectService.Put RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) putObject(ctx context.Context) (objectWriter, error) {
-	var err error
-	var stream corePutObjectStream
-	stream.reqWriter, err = rpcapi.PutObject((*client.Client)(x), &stream.resp, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return &stream, nil
-}
-
-// executes ObjectService.Delete RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) deleteObject(ctx context.Context, req object.DeleteRequest) (*object.DeleteResponse, error) {
-	resp, err := rpcapi.DeleteObject((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes ObjectService.GetRangeHash RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) hashObjectPayloadRanges(ctx context.Context, req object.GetRangeHashRequest) (*object.GetRangeHashResponse, error) {
-	resp, err := rpcapi.HashObjectRange((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes ObjectService.Head RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) headObject(ctx context.Context, req object.HeadRequest) (*object.HeadResponse, error) {
-	resp, err := rpcapi.HeadObject((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return resp, nil
-}
-
-// executes ObjectService.Get RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) getObject(ctx context.Context, req object.GetRequest) (getObjectResponseStream, error) {
-	stream, err := rpcapi.GetObject((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return stream, nil
-}
-
-// executes ObjectService.GetRange RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) getObjectPayloadRange(ctx context.Context, req object.GetRangeRequest) (getObjectPayloadRangeResponseStream, error) {
-	stream, err := rpcapi.GetObjectRange((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return stream, nil
-}
-
-// executes ObjectService.Search RPC declared in NeoFS API protocol
-// using underlying client.Client.
-func (x *coreServer) searchObjects(ctx context.Context, req object.SearchRequest) (searchObjectsResponseStream, error) {
-	stream, err := rpcapi.SearchObjects((*client.Client)(x), &req, client.WithContext(ctx))
-	if err != nil {
-		return nil, rpcErr(err)
-	}
-	return stream, nil
 }

commit 3de0e41d6c45f3a4455bae384cbb95ef49f10492

client: Do not lose transport errors in statistics
Regression from b75db12121973c8ed9b516294bd6bc6bd0f29e46. Since then, no
error were submitted to stat handler of following `Client` ops:
 - `NetmapSnapshot`;
 - `ObjectDelete`.

Now unit tests detect such things.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

client/netmap.go
@@ -250,7 +250,8 @@ func (c *Client) NetMapSnapshot(ctx context.Context, _ PrmNetMapSnapshot) (netma
 
 	resp, err := c.netmap.NetmapSnapshot(ctx, req.ToGRPCMessage().(*protonetmap.NetmapSnapshotRequest))
 	if err != nil {
-		return netmap.NetMap{}, rpcErr(err)
+		err = rpcErr(err)
+		return netmap.NetMap{}, err
 	}
 	var respV2 v2netmap.SnapshotResponse
 	if err = respV2.FromGRPCMessage(resp); err != nil {

client/object_delete.go
@@ -110,7 +110,8 @@ func (c *Client) ObjectDelete(ctx context.Context, containerID cid.ID, objectID 
 
 	resp, err := c.object.Delete(ctx, req.ToGRPCMessage().(*protoobject.DeleteRequest))
 	if err != nil {
-		return oid.ID{}, rpcErr(err)
+		err = rpcErr(err)
+		return oid.ID{}, err
 	}
 	var respV2 v2object.DeleteResponse
 	if err = respV2.FromGRPCMessage(resp); err != nil {

client/object_get.go
@@ -407,7 +407,8 @@ func (c *Client) ObjectHead(ctx context.Context, containerID cid.ID, objectID oi
 
 	resp, err := c.object.Head(ctx, req.ToGRPCMessage().(*protoobject.HeadRequest))
 	if err != nil {
-		return nil, rpcErr(err)
+		err = rpcErr(err)
+		return nil, err
 	}
 	var respV2 v2object.HeadResponse
 	if err = respV2.FromGRPCMessage(resp); err != nil {