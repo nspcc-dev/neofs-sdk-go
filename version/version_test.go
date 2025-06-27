bb785cf9343a79a0cb7e018a9de1687ddadc3981
Pavel Karpy <carpawell@nspcc.ru>
2025-06-27 14:08:54 +0300

    bump version to 2.18
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

--- version/version_test.go
+++ version/version_test.go
@@ -120,7 +120,7 @@ func TestVersion_UnmarshalJSON(t *testing.T) {
 }
 
 func TestEncodeToString(t *testing.T) {
-	require.Equal(t, "v2.16", version.EncodeToString(version.Current()))
+	require.Equal(t, "v2.18", version.EncodeToString(version.Current()))
 	require.Equal(t, "v96984596.1910541418", version.EncodeToString(validVersion))
 }
 
@@ -135,5 +135,5 @@ func TestCurrent(t *testing.T) {
 	v := version.Current()
 
 	require.EqualValues(t, 2, v.Major())
-	require.EqualValues(t, 16, v.Minor())
+	require.EqualValues(t, 18, v.Minor())
 }
