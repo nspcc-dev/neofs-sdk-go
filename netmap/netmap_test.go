nspcc-dev/neofs-sdk-go

commit ca523f1ff10294a82a14f9f3dcb8023d43190562

[#227] netmap: Refactor and document package functionality
Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

netmap/netmap.go
@@ -1,111 +1,39 @@
 package netmap
 
 import (
-	"bytes"
 	"fmt"
-	"strconv"
 
 	"github.com/nspcc-dev/hrw"
 	"github.com/nspcc-dev/neofs-api-go/v2/netmap"
 )
 
-const defaultCBF = 3
-
-var _, _ hrw.Hasher = NodeInfo{}, nodes{}
-
-// Hash implements hrw.Hasher interface.
-//
-// Hash is needed to support weighted HRW therefore sort function sorts nodes
-// based on their public key.
-func (i NodeInfo) Hash() uint64 {
-	return hrw.Hash(i.m.GetPublicKey())
-}
-
-func (i NodeInfo) less(i2 NodeInfo) bool {
-	return bytes.Compare(i.PublicKey(), i2.PublicKey()) < 0
-}
-
-// attribute returns value of the node attribute by the given key. Returns empty
-// string if attribute is missing.
-//
-// Method is needed to internal placement needs.
-func (i NodeInfo) attribute(key string) string {
-	as := i.m.GetAttributes()
-	for j := range as {
-		if as[j].GetKey() == key {
-			return as[j].GetValue()
-		}
-	}
-
-	return ""
-}
-
-func (i *NodeInfo) syncAttributes() {
-	as := i.m.GetAttributes()
-	for j := range as {
-		switch as[j].GetKey() {
-		case AttrPrice:
-			i.priceAttr, _ = strconv.ParseUint(as[j].GetValue(), 10, 64)
-		case AttrCapacity:
-			i.capAttr, _ = strconv.ParseUint(as[j].GetValue(), 10, 64)
-		}
-	}
-}
-
-func (i *NodeInfo) setPrice(price uint64) {
-	i.priceAttr = price
-
-	as := i.m.GetAttributes()
-	for j := range as {
-		if as[j].GetKey() == AttrPrice {
-			as[j].SetValue(strconv.FormatUint(i.capAttr, 10))
-			return
-		}
-	}
-
-	as = append(as, netmap.Attribute{})
-	as[len(as)-1].SetKey(AttrPrice)
-	as[len(as)-1].SetValue(strconv.FormatUint(i.capAttr, 10))
-
-	i.m.SetAttributes(as)
-}
-
-func (i *NodeInfo) price() uint64 {
-	return i.priceAttr
-}
-
-func (i *NodeInfo) setCapacity(capacity uint64) {
-	i.capAttr = capacity
-
-	as := i.m.GetAttributes()
-	for j := range as {
-		if as[j].GetKey() == AttrCapacity {
-			as[j].SetValue(strconv.FormatUint(i.capAttr, 10))
-			return
-		}
-	}
-
-	as = append(as, netmap.Attribute{})
-	as[len(as)-1].SetKey(AttrCapacity)
-	as[len(as)-1].SetValue(strconv.FormatUint(i.capAttr, 10))
-
-	i.m.SetAttributes(as)
-}
-
-func (i NodeInfo) capacity() uint64 {
-	return i.capAttr
-}
-
-// Netmap represents netmap which contains preprocessed nodes.
-type Netmap struct {
+// NetMap represents NeoFS network map. It includes information about all
+// storage nodes registered in NeoFS the network.
+type NetMap struct {
 	nodes []NodeInfo
 }
 
-func (m *Netmap) SetNodes(nodes []NodeInfo) {
+// SetNodes sets information list about all storage nodes from the NeoFS network.
+//
+// Argument MUST NOT be mutated, make a copy first.
+//
+// See also Nodes.
+func (m *NetMap) SetNodes(nodes []NodeInfo) {
 	m.nodes = nodes
 }
 
+// Nodes returns nodes set using SetNodes.
+//
+// Return value MUST not be mutated, make a copy first.
+func (m NetMap) Nodes() []NodeInfo {
+	return m.nodes
+}
+
+// nodes is a slice of NodeInfo instances needed for HRW sorting.
 type nodes []NodeInfo
+
+// assert nodes type provides hrw.Hasher required for HRW sorting.
+var _ hrw.Hasher = nodes{}
 
 // Hash is a function from hrw.Hasher interface. It is implemented
 // to support weighted hrw sorting of buckets. Each bucket is already sorted by hrw,
@@ -143,8 +71,12 @@ func flattenNodes(ns []nodes) nodes {
 	return result
 }
 
-// GetPlacementVectors returns placement vectors for an object given containerNodes cnt.
-func (m *Netmap) GetPlacementVectors(vectors [][]NodeInfo, pivot []byte) ([][]NodeInfo, error) {
+// PlacementVectors sorts container nodes returned by ContainerNodes method
+// and returns placement vectors for the entity identified by the given pivot.
+// For example,in order to build node list to store the object, binary-encoded
+// object identifier can be used as pivot.  Result is deterministic for
+// the fixed NetMap and parameters.
+func (m NetMap) PlacementVectors(vectors [][]NodeInfo, pivot []byte) ([][]NodeInfo, error) {
 	h := hrw.Hash(pivot)
 	wf := defaultWeightFunc(m.nodes)
 	result := make([][]NodeInfo, len(vectors))
@@ -158,13 +90,18 @@ func (m *Netmap) GetPlacementVectors(vectors [][]NodeInfo, pivot []byte) ([][]No
 	return result, nil
 }
 
-// GetContainerNodes returns nodes corresponding to each replica.
-// Order of returned nodes corresponds to order of replicas in p.
-// pivot is a seed for HRW sorting.
-func (m *Netmap) GetContainerNodes(p *PlacementPolicy, pivot []byte) ([][]NodeInfo, error) {
+// ContainerNodes returns two-dimensional list of nodes as a result of applying
+// given PlacementPolicy to the NetMap. Each line of the list corresponds to a
+// replica descriptor. Line order corresponds to order of ReplicaDescriptor list
+// in the policy. Nodes are pre-filtered according to the Filter list from
+// the policy, and then selected by Selector list. Result is deterministic for
+// the fixed NetMap and parameters.
+//
+// Result can be used in PlacementVectors.
+func (m NetMap) ContainerNodes(p PlacementPolicy, pivot []byte) ([][]NodeInfo, error) {
 	c := newContext(m)
 	c.setPivot(pivot)
-	c.setCBF(p.ContainerBackupFactor())
+	c.setCBF(p.backupFactor)
 
 	if err := c.processFilters(p); err != nil {
 		return nil, err
@@ -174,14 +111,15 @@ func (m *Netmap) GetContainerNodes(p *PlacementPolicy, pivot []byte) ([][]NodeIn
 		return nil, err
 	}
 
-	result := make([][]NodeInfo, len(p.Replicas()))
+	result := make([][]NodeInfo, len(p.replicas))
 
-	for i, r := range p.Replicas() {
-		if r.Selector() == "" {
-			if len(p.Selectors()) == 0 {
-				s := new(Selector)
-				s.SetCount(r.Count())
-				s.SetFilter(MainFilterName)
+	for i := range p.replicas {
+		sName := p.replicas[i].GetSelector()
+		if sName == "" {
+			if len(p.selectors) == 0 {
+				var s netmap.Selector
+				s.SetCount(p.replicas[i].GetCount())
+				s.SetFilter(mainFilterName)
 
 				nodes, err := c.getSelection(p, s)
 				if err != nil {
@@ -191,16 +129,16 @@ func (m *Netmap) GetContainerNodes(p *PlacementPolicy, pivot []byte) ([][]NodeIn
 				result[i] = flattenNodes(nodes)
 			}
 
-			for _, s := range p.Selectors() {
-				result[i] = append(result[i], flattenNodes(c.Selections[s.Name()])...)
+			for i := range p.selectors {
+				result[i] = append(result[i], flattenNodes(c.selections[p.selectors[i].GetName()])...)
 			}
 
 			continue
 		}
 
-		nodes, ok := c.Selections[r.Selector()]
+		nodes, ok := c.selections[sName]
 		if !ok {
-			return nil, fmt.Errorf("%w: REPLICA '%s'", ErrSelectorNotFound, r.Selector())
+			return nil, fmt.Errorf("selector not found: REPLICA '%s'", sName)
 		}
 
 		result[i] = append(result[i], flattenNodes(nodes)...)

client/netmap.go
@@ -2,6 +2,8 @@ package client
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	v2netmap "github.com/nspcc-dev/neofs-api-go/v2/netmap"
 	rpcapi "github.com/nspcc-dev/neofs-api-go/v2/rpc"
@@ -96,7 +98,22 @@ func (c *Client) EndpointInfo(ctx context.Context, prm PrmEndpointInfo) (*ResEnd
 			ver.ReadFromV2(*v2ver)
 		}
 		res.setLatestVersion(&ver)
-		res.setNodeInfo(netmap.NewNodeInfoFromV2(body.GetNodeInfo()))
+
+		nodeV2 := body.GetNodeInfo()
+		if nodeV2 == nil {
+			cc.err = errors.New("missing node info in response body")
+			return
+		}
+
+		var node netmap.NodeInfo
+
+		cc.err = node.ReadFromV2(*nodeV2)
+		if cc.err != nil {
+			cc.err = fmt.Errorf("invalid node info: %w", cc.err)
+			return
+		}
+
+		res.setNodeInfo(&node)
 	}
 
 	// process call
@@ -171,7 +188,21 @@ func (c *Client) NetworkInfo(ctx context.Context, prm PrmNetworkInfo) (*ResNetwo
 	cc.result = func(r responseV2) {
 		resp := r.(*v2netmap.NetworkInfoResponse)
 
-		res.setInfo(netmap.NewNetworkInfoFromV2(resp.GetBody().GetNetworkInfo()))
+		netInfoV2 := resp.GetBody().GetNetworkInfo()
+		if netInfoV2 == nil {
+			cc.err = errors.New("missing network info in response body")
+			return
+		}
+
+		var netInfo netmap.NetworkInfo
+
+		cc.err = netInfo.ReadFromV2(*netInfoV2)
+		if cc.err != nil {
+			cc.err = fmt.Errorf("invalid network info: %w", cc.err)
+			return
+		}
+
+		res.setInfo(&netInfo)
 	}
 
 	// process call

container/container.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/nspcc-dev/neofs-api-go/v2/container"
+	v2netmap "github.com/nspcc-dev/neofs-api-go/v2/netmap"
 	"github.com/nspcc-dev/neofs-api-go/v2/refs"
 	"github.com/nspcc-dev/neofs-sdk-go/acl"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
@@ -158,11 +159,30 @@ func (c *Container) SetAttributes(v Attributes) {
 }
 
 func (c *Container) PlacementPolicy() *netmap.PlacementPolicy {
-	return netmap.NewPlacementPolicyFromV2(c.v2.GetPlacementPolicy())
+	m := c.v2.GetPlacementPolicy()
+	if m == nil {
+		return nil
+	}
+
+	var p netmap.PlacementPolicy
+	// FIXME(@cthulhu-rider): #225 handle error
+	err := p.ReadFromV2(*m)
+	if err != nil {
+		panic(err)
+	}
+
+	return &p
 }
 
 func (c *Container) SetPlacementPolicy(v *netmap.PlacementPolicy) {
-	c.v2.SetPlacementPolicy(v.ToV2())
+	var m *v2netmap.PlacementPolicy
+
+	if v != nil {
+		m = new(v2netmap.PlacementPolicy)
+		v.WriteToV2(m)
+	}
+
+	c.v2.SetPlacementPolicy(m)
 }
 
 // SessionToken returns token of the session within

container/container_test.go
@@ -29,7 +29,7 @@ func TestNewContainer(t *testing.T) {
 	attrs := containertest.Attributes()
 	c.SetAttributes(attrs)
 
-	c.SetPlacementPolicy(policy)
+	c.SetPlacementPolicy(&policy)
 	c.SetNonceUUID(nonce)
 	c.SetOwnerID(ownerID)
 
@@ -39,7 +39,7 @@ func TestNewContainer(t *testing.T) {
 	v2 := c.ToV2()
 	newContainer := container.NewContainerFromV2(v2)
 
-	require.EqualValues(t, newContainer.PlacementPolicy(), policy)
+	require.EqualValues(t, newContainer.PlacementPolicy(), &policy)
 	require.EqualValues(t, newContainer.Attributes(), attrs)
 	require.EqualValues(t, newContainer.BasicACL(), acl.PublicBasicRule)
 

container/test/generate.go
@@ -32,7 +32,8 @@ func Container() *container.Container {
 	x.SetAttributes(Attributes())
 	x.SetOwnerID(usertest.ID())
 	x.SetBasicACL(123)
-	x.SetPlacementPolicy(netmaptest.PlacementPolicy())
+	p := netmaptest.PlacementPolicy()
+	x.SetPlacementPolicy(&p)
 
 	return x
 }

netmap/aggregator.go
@@ -56,7 +56,7 @@ var (
 // capacity and price.
 func newWeightFunc(capNorm, priceNorm normalizer) weightFunc {
 	return func(n NodeInfo) float64 {
-		return capNorm.Normalize(float64(n.capacity())) * priceNorm.Normalize(float64(n.price()))
+		return capNorm.Normalize(float64(n.capacity())) * priceNorm.Normalize(float64(n.Price()))
 	}
 }
 

netmap/clause.go
@@ -1,69 +1,0 @@
-package netmap
-
-import (
-	"github.com/nspcc-dev/neofs-api-go/v2/netmap"
-)
-
-// Clause is an enumeration of selector modifiers
-// that shows how the node set will be formed.
-type Clause uint32
-
-const (
-	ClauseUnspecified Clause = iota
-
-	// ClauseSame is a selector modifier to select only nodes having the same value of bucket attribute.
-	ClauseSame
-
-	// ClauseDistinct is a selector modifier to select nodes having different values of bucket attribute.
-	ClauseDistinct
-)
-
-// ClauseFromV2 converts v2 Clause to Clause.
-func ClauseFromV2(c netmap.Clause) Clause {
-	switch c {
-	default:
-		return ClauseUnspecified
-	case netmap.Same:
-		return ClauseSame
-	case netmap.Distinct:
-		return ClauseDistinct
-	}
-}
-
-// ToV2 converts Clause to v2 Clause.
-func (c Clause) ToV2() netmap.Clause {
-	switch c {
-	default:
-		return netmap.UnspecifiedClause
-	case ClauseDistinct:
-		return netmap.Distinct
-	case ClauseSame:
-		return netmap.Same
-	}
-}
-
-// String returns string representation of Clause.
-//
-// String mapping:
-//  * ClauseDistinct: DISTINCT;
-//  * ClauseSame: SAME;
-//  * ClauseUnspecified, default: CLAUSE_UNSPECIFIED.
-func (c Clause) String() string {
-	return c.ToV2().String()
-}
-
-// FromString parses Clause from a string representation.
-// It is a reverse action to String().
-//
-// Returns true if s was parsed successfully.
-func (c *Clause) FromString(s string) bool {
-	var g netmap.Clause
-
-	ok := g.FromString(s)
-
-	if ok {
-		*c = ClauseFromV2(g)
-	}
-
-	return ok
-}

netmap/clause_test.go
@@ -1,43 +1,0 @@
-package netmap
-
-import (
-	"testing"
-
-	"github.com/nspcc-dev/neofs-api-go/v2/netmap"
-	"github.com/stretchr/testify/require"
-)
-
-func TestClauseFromV2(t *testing.T) {
-	for _, item := range []struct {
-		c   Clause
-		cV2 netmap.Clause
-	}{
-		{
-			c:   ClauseUnspecified,
-			cV2: netmap.UnspecifiedClause,
-		},
-		{
-			c:   ClauseSame,
-			cV2: netmap.Same,
-		},
-		{
-			c:   ClauseDistinct,
-			cV2: netmap.Distinct,
-		},
-	} {
-		require.Equal(t, item.c, ClauseFromV2(item.cV2))
-		require.Equal(t, item.cV2, item.c.ToV2())
-	}
-}
-
-func TestClause_String(t *testing.T) {
-	toPtr := func(v Clause) *Clause {
-		return &v
-	}
-
-	testEnumStrings(t, new(Clause), []enumStringItem{
-		{val: toPtr(ClauseDistinct), str: "DISTINCT"},
-		{val: toPtr(ClauseSame), str: "SAME"},
-		{val: toPtr(ClauseUnspecified), str: "CLAUSE_UNSPECIFIED"},
-	})
-}

commit 085709cb05d33472fdcd352718cbc12e69ac8702

netmap: Fix index out of range panic in `ContainerNodes` method
It could occur when number of SELECT statements exceeded number of REP
ones.

Regression from ca523f1ff10294a82a14f9f3dcb8023d43190562.

Signed-off-by: Leonard Lyubich <leonard@morphbits.io>

netmap/netmap.go
@@ -212,8 +212,8 @@ func (m NetMap) ContainerNodes(p PlacementPolicy, containerID cid.ID) ([][]NodeI
 				result[i] = flattenNodes(nodes)
 			}
 
-			for i := range p.selectors {
-				result[i] = append(result[i], flattenNodes(c.selections[p.selectors[i].Name()])...)
+			for _, s := range p.selectors {
+				result[i] = append(result[i], flattenNodes(c.selections[s.Name()])...)
 			}
 
 			continue

netmap/netmap_test.go
@@ -4,7 +4,9 @@ import (
 	"slices"
 	"testing"
 
+	cidtest "github.com/nspcc-dev/neofs-sdk-go/container/id/test"
 	"github.com/nspcc-dev/neofs-sdk-go/netmap"
+	netmaptest "github.com/nspcc-dev/neofs-sdk-go/netmap/test"
 	protonetmap "github.com/nspcc-dev/neofs-sdk-go/proto/netmap"
 	"github.com/stretchr/testify/require"
 )
@@ -199,4 +201,36 @@ func TestNetMap_SetEpoch(t *testing.T) {
 
 	nm.SetEpoch(anyValidCurrentEpoch + 1)
 	require.EqualValues(t, anyValidCurrentEpoch+1, nm.Epoch())
+}
+
+func TestNetMap_ContainerNodes(t *testing.T) {
+	t.Run("unlinked selectors", func(t *testing.T) {
+		t.Run("more than REP rules", func(t *testing.T) {
+			nodes := nNodes(5)
+			const ps = "REP 1 CBF 1 SELECT 1 FROM * SELECT 1 FROM *"
+
+			var p netmap.PlacementPolicy
+			require.NoError(t, p.DecodeString(ps))
+
+			var nm netmap.NetMap
+			nm.SetNodes(nodes)
+
+			res, err := nm.ContainerNodes(p, cidtest.ID())
+			require.NoError(t, err)
+			require.Len(t, res, 1) // num of REP statements
+			require.Len(t, res[0], 2)
+			for _, set := range res {
+				require.Len(t, set, 2)
+				require.Subset(t, nodes, set)
+			}
+		})
+	})
+}
+
+func nNodes(n int) []netmap.NodeInfo {
+	res := make([]netmap.NodeInfo, n)
+	for i := range res {
+		res[i] = netmaptest.NodeInfo()
+	}
+	return res
 }