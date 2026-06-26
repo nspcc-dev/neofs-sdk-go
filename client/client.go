b4f7077d37d1fd90fed8bdce35debfc20f7cf2e9
Roman Khimov <roman@nspcc.ru>
2026-06-26 16:11:56 +0300

    client: read/write more to/from grpc
    Follow https://github.com/nspcc-dev/neofs-node/pull/4130 for other clients
    like gateways.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- client/client.go
+++ client/client.go
@@ -210,6 +210,8 @@ func (c *Client) Dial(prm PrmDial) error {
 		grpc.WithReturnConnectionError(),
 		grpc.FailOnNonTempDialError(true),
 		grpc.WithContextDialer(prm.customConnFunc),
+		grpc.WithReadBufferSize(256*1024),
+		grpc.WithWriteBufferSize(256*1024),
 	)
 	if err != nil {
 		return fmt.Errorf("gRPC dial: %w", err)
